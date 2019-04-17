5c0148ac33ab1e0f0a81516db3f473822de0dd99
Janhavi Mahajan <jamahaja@microsoft.com>
2019-04-17 16:20:38 -0400

    feat(data prep) NLTK tokenizer util file and notebook, deleted some redundant files, updated snli util with cleaner data prep functions

---
+++ utils_nlp/nltk/nltk_utils.py
@@ -0,0 +1,9 @@
+import nltk
+
+def nltk_tokenizer(snli_df):
+    snli_df['sentence1_tokens'] = snli_df.apply(lambda row: nltk.word_tokenize(row['sentence1']), axis=1)
+    snli_df['sentence2_tokens'] = snli_df.apply(lambda row: nltk.word_tokenize(row['sentence2']), axis=1)
+
+    return snli_df
+
+
