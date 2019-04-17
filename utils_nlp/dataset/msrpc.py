ac0abdfd61dbb676971152718793b9a47c51116e
Abhiram E <abeswara@microsoft.com>
2019-04-17 16:30:58 -0400

    Data loader for MSR PC 1. Added data downloader for MSR PC 2. Added support to clean data and load specified datasets as a pandas dataframe. 3. Updates to environment.yml for newly added packages.

---
+++ utils_nlp/dataset/msrpc.py
@@ -0,0 +1,60 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+
+import os
+import pathlib
+
+import pandas as pd
+
+from utils_nlp.dataset.url_utils import maybe_download, download_path
+
+DATASET_DICT = {
+    'train': 'msr_paraphrase_train.txt',
+    'test': 'msr_paraphrase_test.txt',
+    'all': 'msr_paraphrase_data.txt'
+}
+
+
+def download_msrpc(download_dir):
+    """Downloads Windows Installer for Microsoft Paraphrase Corpus.
+    Args:
+        download_dir (str): File path for the downloaded file
+
+    Returns:
+        file_path to the downloaded dataset.
+    """
+
+    url = "https://download.microsoft.com/download/D/4/6/D46FF87A-F6B9-4252-AA8B-3604ED519838/MSRParaphraseCorpus.msi"
+    return maybe_download(url, work_directory=download_dir)
+
+
+def load_pandas_df(local_cache_path=None, dataset_type='train'):
+    """Load pandas dataframe and clean the data from the downloaded dataset
+
+    Args:
+        dataset_type (str): Key to the DATASET_DICT item. Loads the dataset specified. Could be train or test.
+        local_cache_path (str): Path to download the dataset installer.
+
+    Returns:
+        A pandas dataframe with 3 columns, Sentence 1, Sentence 2 and score.
+
+    """
+
+    if dataset_type not in DATASET_DICT.keys():
+        raise Exception("Dataset type not found!")
+
+    with download_path(local_cache_path) as path:
+        path = pathlib.Path(path)
+        installer_datapath = download_msrpc(path)
+        print("The Windows Installer for Mircosoft Paraphrase Corpus has been downloaded at ", installer_datapath)
+        data_directory = input("Please install and provide the installed directory. Thanks! \n")
+
+        data_directory = pathlib.Path(data_directory)
+        assert os.path.exists(data_directory)
+
+        file_path = os.path.join(data_directory, DATASET_DICT[dataset_type])
+        df = pd.read_csv(file_path, delimiter='\t', error_bad_lines=False).drop(columns=['#1 ID', '#2 ID']) \
+            .dropna() \
+            .rename(index=str, columns={"Quality": "score", "#1 String": "sentence1", "#2 String": "sentence2"})
+        return df
--- utils_nlp/dataset/preprocess.py
+++ utils_nlp/dataset/preprocess.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import spacy
 
-def to_lowercase(df):
-	return df.applymap(lambda s: s.lower() if type(s) == str else s)
 
-def to_spacy_tokens(df, custom_stop_words = []):
+def to_lowercase(df):
+    return df.applymap(lambda s: s.lower() if type(s) == str else s)
+
+
+def to_spacy_tokens(df, custom_stop_words=None):
+    if custom_stop_words is None:
+        custom_stop_words = []
     nlp = spacy.load("en_core_web_sm")
     if len(custom_stop_words) > 0:
         for csw in custom_stop_words:
@@ -15,5 +19,3 @@ def to_spacy_tokens(df, custom_stop_words = []):
     tok_df.columns = ['sentence1_tokens', 'sentence2_tokens']
     tokenized = pd.concat([df, tok_df], axis=1)
     return tokenized
-
-
