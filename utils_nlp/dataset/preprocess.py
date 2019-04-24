4aadf666543a5fafcfb91b9d75fe8e8e6c22250a
Janhavi Mahajan <jamahaja@microsoft.com>
2019-04-24 11:28:03 -0400

    feat(code reformat) moved nltk utils to preprocess.py

--- utils_nlp/dataset/preprocess.py
+++ utils_nlp/dataset/preprocess.py
@@ -3,6 +3,8 @@
 
 import pandas as pd
 import spacy
+import nltk
+from nltk.corpus import stopwords
 
 
 def to_lowercase(df):
@@ -59,3 +61,50 @@ def rm_spacy_stopwords(df, custom_stopwords=[]):
     tok_df.columns = ["sentence1_tokens_stop", "sentence2_tokens_stop"]
     tokenized = pd.concat([df, tok_df], axis=1)
     return tokenized
+
+
+def nltk_tokenizer(snli_df):
+    """
+    This function converts a sentence to word tokens using nltk.
+    It adds two new columns sentence1_tokens and sentence2_tokens to the input pandas dataframe
+    Args:
+        snli_df: pandas dataframe
+
+    Returns:
+        pandas dataframe with columns ['score','sentence1', 'sentence2', 'sentence1_tokens', 'sentence2_tokens']
+    """
+    snli_df["sentence1_tokens"] = snli_df.apply(
+        lambda row: nltk.word_tokenize(row["sentence1"]), axis=1
+    )
+    snli_df["sentence2_tokens"] = snli_df.apply(
+        lambda row: nltk.word_tokenize(row["sentence2"]), axis=1
+    )
+
+    return snli_df
+
+
+def nltk_remove_stop_words(snli_df):
+    """
+        This function removes stop words from a sentence using nltk.
+        It adds two new columns sentence1_tokens_stop and sentence2_tokens_stop to the input pandas dataframe
+    Args:
+        snli_df: pandas dataframe
+
+    Returns:
+        pandas dataframe with columns ['score','sentence1', 'sentence2', 'sentence1_tokens', 'sentence2_tokens']
+    """
+    if not {"sentence1_tokens", "sentence2_tokens"}.issubset(snli_df.columns):
+        snli_df = nltk_tokenizer(snli_df)
+
+    stop_words = tuple(stopwords.words("english"))
+
+    snli_df["sentence1_tokens_stop"] = [
+        [word for word in row if word not in stop_words]
+        for row in snli_df["sentence1_tokens"]
+    ]
+    snli_df["sentence2_tokens_stop"] = [
+        [word for word in row if word not in stop_words]
+        for row in snli_df["sentence2_tokens"]
+    ]
+
+    return snli_df
