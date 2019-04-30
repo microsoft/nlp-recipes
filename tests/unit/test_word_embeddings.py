8025b4449d16c7e9a41a72109ecc7f3f9f7b7e24
abeswara <abeswara@microsoft.com>
2019-04-30 13:52:37 -0400

    Glove loader - Code changes and unit tests.
    1. Added methods to download, extract and load glove vectors.
    2. Added units tests to test the public methods.
    
    Other changes
     1. Made download and extract methods private.
     2. Refactored Word2vec unit tests to exclude private methods.

--- tests/unit/test_word_embeddings.py
+++ tests/unit/test_word_embeddings.py
@@ -2,45 +2,41 @@
 # Licensed under the MIT License.
 import os
 import shutil
-import pytest
 from pathlib import Path
 
 from gensim.models.keyedvectors import Word2VecKeyedVectors
 
+from utils_nlp.pretrained_embeddings.glove import (
+    load_pretrained_vectors as load_glove,
+)
 from utils_nlp.pretrained_embeddings.word2vec import (
-    load_pretrained_vectors,
-    download_word2vec_corpus,
-    extract_word2vec_corpus,
+    load_pretrained_vectors as load_word2vec,
 )
 
 
 def test_load_pretrained_vectors_word2vec():
     dir_path = "temp_data/"
-    assert isinstance(load_pretrained_vectors(dir_path), Word2VecKeyedVectors)
-    shutil.rmtree(os.path.join(os.getcwd(), dir_path))
-
-
-def test_download_word2vec_corpus():
-    dir_path = "temp_data/"
-    os.makedirs(dir_path)
-    file_path = Path(download_word2vec_corpus(dir_path))
-    assert file_path.is_file()
-    shutil.rmtree(os.path.join(os.getcwd(), dir_path))
-
-
-def test_extract_word2vec_corpus():
-    dir_path = "temp_data/"
-    os.makedirs(dir_path)
     file_name = "GoogleNews-vectors-negative300.bin"
-    word2vec_file_path = os.path.join(dir_path, file_name)
+    word2vec_file_path = os.path.join(
+        os.path.join(dir_path, "word2vec"), file_name
+    )
 
-    with pytest.raises(Exception) as e_info:
-        extract_word2vec_corpus(dir_path, word2vec_file_path)
-
-    zip_path = download_word2vec_corpus(dir_path)
-    extract_word2vec_corpus(zip_path, word2vec_file_path)
+    assert isinstance(load_word2vec(dir_path), Word2VecKeyedVectors)
 
     file_path = Path(word2vec_file_path)
+    assert file_path.is_file()
+
+    shutil.rmtree(os.path.join(os.getcwd(), dir_path))
+
+
+def test_load_pretrained_vectors_glove():
+    dir_path = "temp_data/"
+    file_name = "glove.840B.300d.txt"
+    glove_file_path = os.path.join(os.path.join(dir_path, "gloVe"), file_name)
+
+    assert isinstance(load_glove(dir_path), Word2VecKeyedVectors)
+
+    file_path = Path(glove_file_path)
     assert file_path.is_file()
 
     shutil.rmtree(os.path.join(os.getcwd(), dir_path))
