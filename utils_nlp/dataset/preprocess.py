338e606c5e8335773bfab5bd0e1cfa0e5d1950e8
Janhavi Mahajan <jamahaja@microsoft.com>
2019-05-01 16:15:40 -0400

    feat(code review comments) refactoring based on Miguel's comments

--- utils_nlp/dataset/preprocess.py
+++ utils_nlp/dataset/preprocess.py
@@ -68,7 +68,7 @@ def to_nltk_tokens(df):
     This function converts a sentence to word tokens using nltk.
     It adds two new columns sentence1_tokens and sentence2_tokens to the input pd.DataFrame
     Args:
-        df: pd.DataFrame
+        df(pd.DataFrame): pandas dataframe
 
     Returns:
         pd.DataFrame with columns ['score','sentence1', 'sentence2', 'sentence1_tokens', 'sentence2_tokens']
@@ -89,13 +89,13 @@ def rm_nltk_stopwords(df):
     This function removes stop words from a sentence using nltk.
     It adds two new columns sentence1_tokens_stop and sentence2_tokens_stop to the input pd.DataFrame
     Args:
-        df: pd.DataFrame
+        df(pd.DataFrame): pandas dataframe
 
     Returns:
         pd.DataFrame with columns ['score','sentence1', 'sentence2', 'sentence1_tokens', 'sentence2_tokens']
     """
     if not {"sentence1_tokens", "sentence2_tokens"}.issubset(df.columns):
-        df = nltk_tokenizer(df)
+        df = to_nltk_tokens(df)
 
     stop_words = tuple(stopwords.words("english"))
 
