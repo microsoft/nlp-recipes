b49857684249c19559835193ee56392abf092817
Abhiram E <abeswara@microsoft.com>
2019-05-03 14:37:16 -0400

    FastText loader - Code changes and unit tests.
    1. Added methods to download, extract and load glove vectors.
    2. Added units test to test the public method.
    
    Other changes
     1. Refactored files to add return types to docstrings.
     2. Minor changes to path variables.

--- tests/unit/test_word_embeddings.py
+++ tests/unit/test_word_embeddings.py
@@ -4,8 +4,12 @@ import os
 import shutil
 from pathlib import Path
 
+from gensim.models.fasttext import FastText
 from gensim.models.keyedvectors import Word2VecKeyedVectors
 
+from utils_nlp.pretrained_embeddings.fasttext import (
+    load_pretrained_vectors as load_fasttext,
+)
 from utils_nlp.pretrained_embeddings.glove import (
     load_pretrained_vectors as load_glove,
 )
@@ -16,14 +20,14 @@ from utils_nlp.pretrained_embeddings.word2vec import (
 
 def test_load_pretrained_vectors_word2vec():
     dir_path = "temp_data/"
-    file_name = "GoogleNews-vectors-negative300.bin"
-    word2vec_file_path = os.path.join(
-        os.path.join(dir_path, "word2vec"), file_name
+    file_path = os.path.join(
+        os.path.join(dir_path, "word2vec"),
+        "GoogleNews-vectors-negative300.bin",
     )
 
     assert isinstance(load_word2vec(dir_path), Word2VecKeyedVectors)
 
-    file_path = Path(word2vec_file_path)
+    file_path = Path(file_path)
     assert file_path.is_file()
 
     shutil.rmtree(os.path.join(os.getcwd(), dir_path))
@@ -31,12 +35,25 @@ def test_load_pretrained_vectors_word2vec():
 
 def test_load_pretrained_vectors_glove():
     dir_path = "temp_data/"
-    file_name = "glove.840B.300d.txt"
-    glove_file_path = os.path.join(os.path.join(dir_path, "gloVe"), file_name)
+    file_path = os.path.join(
+        os.path.join(dir_path, "gloVe"), "glove.840B.300d.txt"
+    )
 
     assert isinstance(load_glove(dir_path), Word2VecKeyedVectors)
 
-    file_path = Path(glove_file_path)
+    file_path = Path(file_path)
+    assert file_path.is_file()
+
+    shutil.rmtree(os.path.join(os.getcwd(), dir_path))
+
+
+def test_load_pretrained_vectors_fasttext():
+    dir_path = "temp_data/"
+    file_path = os.path.join(os.path.join(dir_path, "fastText"), "wiki.en.bin")
+
+    assert isinstance(load_fasttext(dir_path), FastText)
+
+    file_path = Path(file_path)
     assert file_path.is_file()
 
     shutil.rmtree(os.path.join(os.getcwd(), dir_path))
