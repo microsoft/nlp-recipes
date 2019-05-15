52d720e9bf6d02478c91349bb1b7c1e1a6cfbd90
Abhiram E <abeswara@microsoft.com>
2019-05-15 00:22:37 -0400

    Added option to limit number of word vectors for glove and word2vec

--- tests/unit/test_word_embeddings.py
+++ tests/unit/test_word_embeddings.py
@@ -27,6 +27,9 @@ def test_load_pretrained_vectors_word2vec():
 
     assert isinstance(load_word2vec(dir_path), Word2VecKeyedVectors)
 
+    model = load_word2vec(dir_path, limit=500000)
+    assert isinstance(model, Word2VecKeyedVectors) and (len(model.wv.vocab) == 500000)
+
     file_path = Path(file_path)
     assert file_path.is_file()
 
@@ -40,6 +43,9 @@ def test_load_pretrained_vectors_glove():
     )
 
     assert isinstance(load_glove(dir_path), Word2VecKeyedVectors)
+
+    model = load_glove(dir_path, limit=50000)
+    assert isinstance(model, Word2VecKeyedVectors) and (len(model.wv.vocab) == 50000)
 
     file_path = Path(file_path)
     assert file_path.is_file()
