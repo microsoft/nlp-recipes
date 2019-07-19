ddf810fa9f8bced03f2c7595b9d7035082196be7
Abhiram E <abeswara@microsoft.com>
2019-07-19 18:31:13 -0400

    Fixed some messed conflict resolution

--- tests/integration/test_notebooks_sentence_similarity.py
+++ tests/integration/test_notebooks_sentence_similarity.py
@@ -11,7 +11,29 @@ from utils_nlp.azureml.azureml_utils import get_or_create_workspace
 from tests.notebooks_common import OUTPUT_NOTEBOOK
 
 sys.path.append("../../")
-ABS_TOL = 0.1
+ABS_TOL = 0.2
+
+
+@pytest.fixture(scope="module")
+def baseline_results():
+    return {
+        "Word2vec Cosine": 0.6476606845766778,
+        "Word2vec Cosine with Stop Words": 0.6683808069062863,
+        "Word2vec WMD": 0.6574175839579567,
+        "Word2vec WMD with Stop Words": 0.6574175839579567,
+        "GLoVe Cosine": 0.6688056947022161,
+        "GLoVe Cosine with Stop Words": 0.6049380247374541,
+        "GLoVe WMD": 0.6267300417407605,
+        "GLoVe WMD with Stop Words": 0.48470008225931194,
+        "fastText Cosine": 0.6707510007525627,
+        "fastText Cosine with Stop Words": 0.6771300330824099,
+        "fastText WMD": 0.6394958913339955,
+        "fastText WMD with Stop Words": 0.5177829727556036,
+        "TF-IDF Cosine": 0.6749213786510483,
+        "TF-IDF Cosine with Stop Words": 0.7118087132257667,
+        "Doc2vec Cosine": 0.5337078384749167,
+        "Doc2vec Cosine with Stop Words": 0.4498543211602068,
+    }
 
 
 @pytest.mark.notebooks
