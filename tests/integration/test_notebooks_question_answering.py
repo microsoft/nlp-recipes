29a914ee221a5d9dd49f9d7588bd828ee71b6739
catherine667 <shaoliqun89@gmail.com>
2019-07-23 17:46:07 -0400

    add notebook testing for BERT QA

--- tests/integration/test_notebooks_question_answering.py
+++ tests/integration/test_notebooks_question_answering.py
@@ -1,13 +1,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import sys
 import pytest
 import papermill as pm
 import scrapbook as sb
 from tests.notebooks_common import OUTPUT_NOTEBOOK
 
 ABS_TOL = 0.2
+
 
 @pytest.mark.integration
 @pytest.mark.azureml
@@ -32,6 +32,7 @@ def test_bidaf_deep_dive(notebooks,
     result = sb.read_notebook(OUTPUT_NOTEBOOK).scraps.data_dict["validation_EM"]
     assert result == pytest.approx(0.5, abs=ABS_TOL)
 
+
 @pytest.mark.usefixtures("teardown_service")
 @pytest.mark.integration
 @pytest.mark.azureml
@@ -51,3 +52,21 @@ def test_bidaf_quickstart(notebooks,
                                       'webservice_name': "aci-test-service"})
     result = sb.read_notebook(OUTPUT_NOTEBOOK).scraps.data_dict["answer"]
     assert result == "Bi-Directional Attention Flow"
+
+
+@pytest.mark.notebooks
+@pytest.mark.gpu
+def test_bert_qa_runs(notebooks):
+    notebook_path = notebooks["bert_qa_trainer"]
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        parameters=dict(
+            TRAIN_SCRIPT_PATH='../../scenarios/question_answering/bert_run_squad_azureml.py',
+            BERT_MODEL='bert-base-uncased',
+            NUM_TRAIN_EPOCHS=1.0,
+            NODE_COUNT=1,
+            MAX_TOTAL_RUNS=1,
+            MAX_CONCURRENT_RUNS=1,
+        ),
+    )
