f7d79c0a44d0c0d9e153e108ca99fe21a3da1ff2
Daisy Deng <daden@microsoft.com>
2019-07-29 18:39:47 +0000

    use tmpdir fixture

--- tests/unit/app_tests/test_luis_model_loading.py
+++ tests/unit/app_tests/test_luis_model_loading.py
@@ -13,11 +13,11 @@ FILE_NAME = URL.split("/")[-1]
 
 
 @pytest.fixture()
-def luis_model(local_cache_path="."):
+def luis_model(tmpdir):
     print(FILE_NAME)
-    maybe_download(URL, FILE_NAME, local_cache_path)
-    assert os.path.exists(os.path.join(local_cache_path, FILE_NAME))
-    with open(os.path.join(local_cache_path, FILE_NAME)) as fd:
+    maybe_download(URL, FILE_NAME, tmpdir)
+    assert os.path.exists(os.path.join(tmpdir, FILE_NAME))
+    with open(os.path.join(tmpdir, FILE_NAME)) as fd:
         luis_model_obj = json.load(fd)
         return luis_model_obj
     return None
