5770bdabdd428172e336252b1763e47376592b17
Abhiram E <abeswara@microsoft.com>
2019-07-31 18:30:10 -0400

    Reverted changes to AzureML utils and reran the notebook

--- utils_nlp/azureml/azureml_utils.py
+++ utils_nlp/azureml/azureml_utils.py
@@ -1,6 +1,12 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import os
 from azureml.core.authentication import AzureCliAuthentication
 from azureml.core.authentication import InteractiveLoginAuthentication
@@ -30,72 +36,50 @@ def get_or_create_workspace(
     resource_group=None,
     workspace_name=None,
     workspace_region=None,
-):
-    """Get or create AzureML Workspace this will save the config to the path specified for later use
+) -> Workspace:
+    """
+    Returns workspace if one exists already with the name
+    otherwise creates a new one.
 
-    Args:
-        config_path (str): optional directory to look for / store config.json file (defaults to current directory)
-        subscription_id (str): subscription id
-        resource_group (str): resource group
-        workspace_name (str): workspace name
-        workspace_region (str): region
-
-    Returns:
-        Workspace
+    Args
+    config_path: optional directory to look for / store config.json file (defaults to current directory)
+    subscription_id: Azure subscription id
+    resource_group: Azure resource group to create workspace and related resources
+    workspace_name: name of azure ml workspace
+    workspace_region: region for workspace
     """
 
-    # use environment variables if needed
-    if subscription_id is None:
-        subscription_id = os.getenv("SUBSCRIPTION_ID")
-    if resource_group is None:
-        resource_group = os.getenv("RESOURCE_GROUP")
-    if workspace_name is None:
-        workspace_name = os.getenv("WORKSPACE_NAME")
-    if workspace_region is None:
-        workspace_region = os.getenv("WORKSPACE_REGION")
-
-    # define fallback options in order to try
-    options = [
-        (
-            Workspace,
-            dict(
-                subscription_id=subscription_id,
-                resource_group=resource_group,
-                workspace_name=workspace_name,
-            ),
-        ),
-        (Workspace.from_config, dict(path=config_path)),
-        (
-            Workspace.create,
-            dict(
-                subscription_id=subscription_id,
-                resource_group=resource_group,
+    try:
+        # get existing azure ml workspace
+        if config_path is not None:
+            ws = Workspace.from_config(config_path, auth=get_auth())
+        else:
+            ws = Workspace.get(
                 name=workspace_name,
-                location=workspace_region,
-                create_resource_group=True,
-                exist_ok=True,
-            ),
-        ),
-    ]
+                subscription_id=subscription_id,
+                resource_group=resource_group,
+                auth=get_auth(),
+            )
 
-    for function, kwargs in options:
-        try:
-            ws = function(**kwargs)
-            break
-        except Exception:
-            continue
-    else:
-        raise ValueError(
-            "Failed to get or create AzureML Workspace with the configuration information provided"
+    except WorkspaceException:
+        # this call might take a minute or two.
+        print("Creating new workspace")
+        ws = Workspace.create(
+            name=workspace_name,
+            subscription_id=subscription_id,
+            resource_group=resource_group,
+            create_resource_group=True,
+            location=workspace_region,
+            auth=get_auth(),
         )
 
-    ws.write_config(path=config_path)
+        ws.write_config(path=config_path)
     return ws
 
 
 def log_metrics_scalar(value, run, name="", description=None):
     """Log scalar metric to the AzureML run
-    
+
     Args:
         value : numerical or string value to log
         run : AzureML Run object
@@ -107,7 +91,7 @@ def log_metrics_scalar(value, run, name="", description=None):
 
 def log_metrics_table(df, run, name="", description=None, as_scalar=False):
     """Log data from pd.DataFrame to the AzureML run
-    
+
     Args:
         df : pd.DataFrame containing metrics to log
         run : AzureML Run object
