e1bb26fb28f821d0a7d1a63c9391ebda29d06b77
hlums <honglu@microsoft.com>
2019-08-02 20:01:20 +0000

    Add integration test for entailment notebook.

---
+++ tests/integration/test_notebooks_entailment_multinli_bert.py
@@ -0,0 +1,17 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import pytest
+import papermill as pm
+from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
+
+
+@pytest.mark.integration
+def test_entailment_multinli_bert(notebooks):
+    notebook_path = notebooks["entailment_multinli_bert"]
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        parameters={"QUICK_RUN": True},
+        kernel_name=KERNEL_NAME,
+    )
