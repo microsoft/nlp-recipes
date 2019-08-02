50f6175580f65cb23e9303176a38613f9f3bbe28
Abhiram E <abeswara@microsoft.com>
2019-08-02 18:02:00 -0400

    Added changes to allow test loader creation with Sequential

--- utils_nlp/models/bert/sequence_classification_distributed.py
+++ utils_nlp/models/bert/sequence_classification_distributed.py
@@ -27,11 +27,11 @@ class BERTSequenceClassifier:
     """BERT-based sequence classifier"""
 
     def __init__(
-        self,
-        language=Language.ENGLISH,
-        num_labels=2,
-        cache_dir=".",
-        use_distributed=False,
+            self,
+            language=Language.ENGLISH,
+            num_labels=2,
+            cache_dir=".",
+            use_distributed=False,
     ):
 
         """
@@ -86,11 +86,11 @@ class BERTSequenceClassifier:
                 warnings.warn("No GPU available! Using CPU.")
 
     def create_optimizer(
-        self,
-        num_train_optimization_steps,
-        lr=2e-5,
-        fp16_allreduce=False,
-        warmup_proportion=None,
+            self,
+            num_train_optimization_steps,
+            lr=2e-5,
+            fp16_allreduce=False,
+            warmup_proportion=None,
     ):
 
         """
@@ -136,11 +136,12 @@ class BERTSequenceClassifier:
 
         return optimizer
 
-    def create_data_loader(self, dataset, batch_size=32, **kwargs):
+    def create_data_loader(self, dataset, batch_size=32, mode="train", **kwargs):
         """
         Method to create a data loader for a given Tensor dataset.
 
         Args:
+            mode(str): Mode for creating data loader. Could be train or test.
             dataset(torch.utils.data.Dataset): A Tensor dataset.
             batch_size(int): Batch size.
 
@@ -149,15 +150,17 @@ class BERTSequenceClassifier:
 
         """
 
-        if self.use_distributed:
-            train_sampler = torch.utils.data.distributed.DistributedSampler(
+        if mode == "test":
+            sampler = torch.utils.data.sampler.SequentialSampler(dataset)
+        elif self.use_distributed:
+            sampler = torch.utils.data.distributed.DistributedSampler(
                 dataset, num_replicas=hvd.size(), rank=hvd.rank()
             )
         else:
-            train_sampler = torch.utils.data.RandomSampler(dataset)
+            sampler = torch.utils.data.RandomSampler(dataset)
 
         data_loader = torch.utils.data.DataLoader(
-            dataset, batch_size=batch_size, sampler=train_sampler, **kwargs
+            dataset, batch_size=batch_size, sampler=sampler, **kwargs
         )
 
         return data_loader
@@ -185,16 +188,16 @@ class BERTSequenceClassifier:
         model_to_save.config.to_json_file(output_config_file)
 
     def fit(
-        self,
-        train_loader,
-        epoch,
-        bert_optimizer=None,
-        num_epochs=1,
-        num_gpus=0,
-        lr=2e-5,
-        warmup_proportion=None,
-        fp16_allreduce=False,
-        num_train_optimization_steps=10,
+            self,
+            train_loader,
+            epoch,
+            bert_optimizer=None,
+            num_epochs=1,
+            num_gpus=0,
+            lr=2e-5,
+            warmup_proportion=None,
+            fp16_allreduce=False,
+            num_train_optimization_steps=10,
     ):
         """
         Method to fine-tune the bert classifier using the given training data
@@ -287,6 +290,9 @@ class BERTSequenceClassifier:
     def predict(self, test_loader, num_gpus=None, probabilities=False):
         """
 
+        Method to predict the results on the test loader. Only evaluates for non distributed
+        workload on the head node in a distributed setup.
+
         Args:
             test_loader(torch Dataloader): Torch Dataloader created from Torch Dataset
             num_gpus (int, optional): The number of gpus to use.
@@ -300,50 +306,51 @@ class BERTSequenceClassifier:
             1darray, dict(1darray, 1darray, ndarray): Predicted classes and target labels or
                 a dictionary with classes, target labels, probabilities) if probabilities is True.
         """
+        device = get_device("cpu" if num_gpus == 0 else "gpu")
+        self.model = move_to_device(self.model, device, num_gpus)
 
-        if hvd.rank() == 0:
-            device = get_device("cpu" if num_gpus == 0 else "gpu")
-            self.model = move_to_device(self.model, device, num_gpus)
+        # score
+        self.model.eval()
 
-            # score
-            self.model.eval()
+        preds = []
+        test_labels = []
+        for i, data in enumerate(tqdm(test_loader, desc="Iteration")):
+            x_batch = data["token_ids"]
+            x_batch = x_batch.cuda()
 
-            preds = []
-            test_labels = []
-            for i, data in enumerate(tqdm(test_loader, desc="Iteration")):
-                x_batch = data["token_ids"]
-                x_batch = x_batch.cuda()
+            mask_batch = data["input_mask"]
+            mask_batch = mask_batch.cuda()
 
-                mask_batch = data["input_mask"]
-                mask_batch = mask_batch.cuda()
+            y_batch = data["labels"]
 
-                y_batch = data["labels"]
+            token_type_ids_batch = None
+            if (
+                    "token_type_ids" in data
+                    and data["token_type_ids"] is not None
+            ):
+                token_type_ids_batch = data["token_type_ids"]
+                token_type_ids_batch = token_type_ids_batch.cuda()
 
-                token_type_ids_batch = None
-                if "token_type_ids" in data and data["token_type_ids"] is not None:
-                    token_type_ids_batch = data["token_type_ids"]
-                    token_type_ids_batch = token_type_ids_batch.cuda()
+            with torch.no_grad():
+                p_batch = self.model(
+                    input_ids=x_batch,
+                    token_type_ids=token_type_ids_batch,
+                    attention_mask=mask_batch,
+                    labels=None,
+                )
+            preds.append(p_batch.cpu())
+            test_labels.append(y_batch)
 
-                with torch.no_grad():
-                    p_batch = self.model(
-                        input_ids=x_batch,
-                        token_type_ids=token_type_ids_batch,
-                        attention_mask=mask_batch,
-                        labels=None,
-                    )
-                preds.append(p_batch.cpu())
-                test_labels.append(y_batch)
+        preds = np.concatenate(preds)
+        test_labels = np.concatenate(test_labels)
 
-            preds = np.concatenate(preds)
-            test_labels = np.concatenate(test_labels)
-
-            if probabilities:
-                return {
-                    "Predictions": preds.argmax(axis=1),
-                    "Target": test_labels,
-                    "classes probabilities": nn.Softmax(dim=1)(
-                        torch.Tensor(preds)
-                    ).numpy(),
-                }
-            else:
-                return preds.argmax(axis=1), test_labels
+        if probabilities:
+            return {
+                "Predictions": preds.argmax(axis=1),
+                "Target": test_labels,
+                "classes probabilities": nn.Softmax(dim=1)(
+                    torch.Tensor(preds)
+                ).numpy(),
+            }
+        else:
+            return preds.argmax(axis=1), test_labels
