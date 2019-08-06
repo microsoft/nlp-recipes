f276d3cfa560f81fae571133aa228a6900af9b1a
Hong Lu <honglu@microsoft.com>
2019-08-06 17:33:39 -0400

    Add pytest.mark.gpu

--- tests/integration/test_notebooks_entailment_multinli_bert.py
+++ tests/integration/test_notebooks_entailment_multinli_bert.py
@@ -6,14 +6,17 @@ import papermill as pm
 from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
 
 
+@pytest.mark.gpu
 @pytest.mark.integration
 def test_entailment_multinli_bert(notebooks):
     notebook_path = notebooks["entailment_multinli_bert"]
     pm.execute_notebook(
         notebook_path,
         OUTPUT_NOTEBOOK,
-        parameters={"TRAIN_DATA_USED_PERCENT": 0.001, 
-                    "DEV_DATA_USED_PERCENT": 0.01,
-                    "NUM_EPOCHS": 1},
+        parameters={
+            "TRAIN_DATA_USED_PERCENT": 0.001,
+            "DEV_DATA_USED_PERCENT": 0.01,
+            "NUM_EPOCHS": 1,
+        },
         kernel_name=KERNEL_NAME,
     )
