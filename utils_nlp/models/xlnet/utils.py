b7dcc5482721b440a243e368aba512bb9b3f0c75
Abishkar Chhetri <abishkarchhetri@gmail.com>
2019-08-15 15:31:07 +0000

    change confusion matrix to show proportional confusions

--- utils_nlp/models/xlnet/utils.py
+++ utils_nlp/models/xlnet/utils.py
@@ -14,11 +14,11 @@ def generate_confusion_matrix(y_true, y_pred,labels):
         labels: A list of labels
 
     """
-    conf_matrix=np.matrix(confusion_matrix(y_true, y_pred))
+    conf_matrix=np.array(confusion_matrix(y_true, y_pred))
+    conf_matrix=np.divide(conf_matrix.astype('float'), conf_matrix.sum(axis=1)[:,np.newaxis])
     conf_dataframe = pd.DataFrame(conf_matrix,labels,labels)
     dimensions_plot = (8, 5)
     fig, ax = pyplot.subplots(figsize=dimensions_plot)
     sn.set(font_scale=1.1)
     ax=sn.heatmap(conf_dataframe,cmap="Blues", annot=True,annot_kws={"size": 16})
     ax.set(xlabel='True Labels', ylabel='Predicted Labels')
-    
