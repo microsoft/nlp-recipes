e47469dbe106ad620449b141fcc5c1f9b47a2784
hlums <honglu@microsoft.com>
2019-08-15 14:20:20 +0000

    Set weight decay back to 0.01

--- utils_nlp/models/bert/question_answering.py
+++ utils_nlp/models/bert/question_answering.py
@@ -125,7 +125,7 @@ class BERTQAExtractor:
                     for n, p in self.model.named_parameters()
                     if not any(nd in n for nd in no_decay)
                 ],
-                "weight_decay": 0.0,
+                "weight_decay": 0.01,
             },
             {
                 "params": [
