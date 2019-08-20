75443c6344b5bd929dad95fe0aeef2154c9fc547
saidbleik <saidbleik@outlook.com>
2019-08-20 17:43:11 +0000

    rem horovod dep

--- tools/generate_conda_file.py
+++ tools/generate_conda_file.py
@@ -88,10 +88,10 @@ PIP_BASE = {
 PIP_GPU = {}
 
 PIP_DARWIN = {}
-PIP_DARWIN_GPU = {"horovod": "horovod>=0.16.1"}
+PIP_DARWIN_GPU = {}
 
 PIP_LINUX = {}
-PIP_LINUX_GPU = {"horovod": "horovod>=0.16.1"}
+PIP_LINUX_GPU = {}
 CONDA_LINUX = {"cudatoolkit": "cudatoolkit==9.2"}
 
 PIP_WIN32 = {}
