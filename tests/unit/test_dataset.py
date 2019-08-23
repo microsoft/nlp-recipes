b7ca1e3349852c631309b59a2e976e1b2d18b6fa
hlums <honglu@microsoft.com>
2019-08-23 20:29:27 +0000

    Add test for squad dataset.

--- tests/unit/test_dataset.py
+++ tests/unit/test_dataset.py
@@ -14,120 +14,120 @@ from utils_nlp.dataset import squad
 from utils_nlp.dataset.ner_utils import preprocess_conll
 
 
-# @pytest.fixture
-# def ner_utils_test_data(scope="module"):
-#     return {
-#         "input": "The O\n139th I-ORG\nwas O\nformed O\nat O\nCamp I-LOC\n"
-#         "Howe I-LOC\n, O\nnear O\nPittsburgh I-LOC\n, O\non O\n"
-#         "September O\n1 O\n, O\n1862 O\n. O\n\nFrederick I-PER\n"
-#         "H. I-PER\nCollier I-PER\nwas O\nthe O\nfirst O\ncolonel O\n. O",
-#         "expected_output": (
-#             [
-#                 [
-#                     "The",
-#                     "139th",
-#                     "was",
-#                     "formed",
-#                     "at",
-#                     "Camp",
-#                     "Howe",
-#                     ",",
-#                     "near",
-#                     "Pittsburgh",
-#                     ",",
-#                     "on",
-#                     "September",
-#                     "1",
-#                     ",",
-#                     "1862",
-#                     ".",
-#                 ],
-#                 [
-#                     "Frederick",
-#                     "H.",
-#                     "Collier",
-#                     "was",
-#                     "the",
-#                     "first",
-#                     "colonel",
-#                     ".",
-#                 ],
-#             ],
-#             [
-#                 [
-#                     "O",
-#                     "I-ORG",
-#                     "O",
-#                     "O",
-#                     "O",
-#                     "I-LOC",
-#                     "I-LOC",
-#                     "O",
-#                     "O",
-#                     "I-LOC",
-#                     "O",
-#                     "O",
-#                     "O",
-#                     "O",
-#                     "O",
-#                     "O",
-#                     "O",
-#                 ],
-#                 ["I-PER", "I-PER", "I-PER", "O", "O", "O", "O", "O"],
-#             ],
-#         ),
-#     }
+@pytest.fixture
+def ner_utils_test_data(scope="module"):
+    return {
+        "input": "The O\n139th I-ORG\nwas O\nformed O\nat O\nCamp I-LOC\n"
+        "Howe I-LOC\n, O\nnear O\nPittsburgh I-LOC\n, O\non O\n"
+        "September O\n1 O\n, O\n1862 O\n. O\n\nFrederick I-PER\n"
+        "H. I-PER\nCollier I-PER\nwas O\nthe O\nfirst O\ncolonel O\n. O",
+        "expected_output": (
+            [
+                [
+                    "The",
+                    "139th",
+                    "was",
+                    "formed",
+                    "at",
+                    "Camp",
+                    "Howe",
+                    ",",
+                    "near",
+                    "Pittsburgh",
+                    ",",
+                    "on",
+                    "September",
+                    "1",
+                    ",",
+                    "1862",
+                    ".",
+                ],
+                [
+                    "Frederick",
+                    "H.",
+                    "Collier",
+                    "was",
+                    "the",
+                    "first",
+                    "colonel",
+                    ".",
+                ],
+            ],
+            [
+                [
+                    "O",
+                    "I-ORG",
+                    "O",
+                    "O",
+                    "O",
+                    "I-LOC",
+                    "I-LOC",
+                    "O",
+                    "O",
+                    "I-LOC",
+                    "O",
+                    "O",
+                    "O",
+                    "O",
+                    "O",
+                    "O",
+                    "O",
+                ],
+                ["I-PER", "I-PER", "I-PER", "O", "O", "O", "O", "O"],
+            ],
+        ),
+    }
 
 
-# def test_maybe_download():
-#     # ToDo: Change this url when repo goes public.
-#     file_url = (
-#         "https://raw.githubusercontent.com/Microsoft/Recommenders/"
-#         "master/LICENSE"
-#     )
-#     filepath = "license.txt"
-#     assert not os.path.exists(filepath)
-#     filepath = maybe_download(file_url, "license.txt", expected_bytes=1162)
-#     assert os.path.exists(filepath)
-#     os.remove(filepath)
-#     with pytest.raises(IOError):
-#         filepath = maybe_download(file_url, "license.txt", expected_bytes=0)
+def test_maybe_download():
+    # ToDo: Change this url when repo goes public.
+    file_url = (
+        "https://raw.githubusercontent.com/Microsoft/Recommenders/"
+        "master/LICENSE"
+    )
+    filepath = "license.txt"
+    assert not os.path.exists(filepath)
+    filepath = maybe_download(file_url, "license.txt", expected_bytes=1162)
+    assert os.path.exists(filepath)
+    os.remove(filepath)
+    with pytest.raises(IOError):
+        filepath = maybe_download(file_url, "license.txt", expected_bytes=0)
 
 
-# def test_msrpc():
-#     with pytest.raises(Exception):
-#         msrpc.load_pandas_df(dataset_type="Dummy")
+def test_msrpc():
+    with pytest.raises(Exception):
+        msrpc.load_pandas_df(dataset_type="Dummy")
 
 
-# def test_wikigold(tmp_path):
-#     wg_sentence_count = 1841
-#     wg_test_percentage = 0.5
-#     wg_test_sentence_count = round(wg_sentence_count * wg_test_percentage)
-#     wg_train_sentence_count = wg_sentence_count - wg_test_sentence_count
+def test_wikigold(tmp_path):
+    wg_sentence_count = 1841
+    wg_test_percentage = 0.5
+    wg_test_sentence_count = round(wg_sentence_count * wg_test_percentage)
+    wg_train_sentence_count = wg_sentence_count - wg_test_sentence_count
 
-#     downloaded_file = os.path.join(tmp_path, "wikigold.conll.txt")
-#     assert not os.path.exists(downloaded_file)
+    downloaded_file = os.path.join(tmp_path, "wikigold.conll.txt")
+    assert not os.path.exists(downloaded_file)
 
-#     train_df, test_df = wikigold.load_train_test_dfs(
-#         tmp_path, test_percentage=wg_test_percentage
-#     )
+    train_df, test_df = wikigold.load_train_test_dfs(
+        tmp_path, test_percentage=wg_test_percentage
+    )
 
-#     assert os.path.exists(downloaded_file)
+    assert os.path.exists(downloaded_file)
 
-#     assert train_df.shape == (wg_train_sentence_count, 2)
-#     assert test_df.shape == (wg_test_sentence_count, 2)
-
-
-# def test_ner_utils(ner_utils_test_data):
-#     output = preprocess_conll(ner_utils_test_data["input"])
-#     assert output == ner_utils_test_data["expected_output"]
+    assert train_df.shape == (wg_train_sentence_count, 2)
+    assert test_df.shape == (wg_test_sentence_count, 2)
 
 
-# def test_xnli(tmp_path):
-#     # Only test for the dev df as the train dataset takes several
-#     # minutes to download
-#     dev_df = xnli.load_pandas_df(local_cache_path=tmp_path, file_split="dev")
-#     assert dev_df.shape == (2490, 2)
+def test_ner_utils(ner_utils_test_data):
+    output = preprocess_conll(ner_utils_test_data["input"])
+    assert output == ner_utils_test_data["expected_output"]
+
+
+def test_xnli(tmp_path):
+    # Only test for the dev df as the train dataset takes several
+    # minutes to download
+    dev_df = xnli.load_pandas_df(local_cache_path=tmp_path, file_split="dev")
+    assert dev_df.shape == (2490, 2)
 
 
 def test_snli(tmp_path):
