ac7c13d6e53c2edb557d243993a1fa1653d1efdc
Amit Gupte <abishkarchhetri@gmail.com>
2019-09-04 17:35:24 -0400

    Moved Confusion Matrix function to utils_nlp/eval and addressed PR comments

--- utils_nlp/models/xlnet/sequence_classification.py
+++ utils_nlp/models/xlnet/sequence_classification.py
@@ -2,18 +2,16 @@ import numpy as np
 from collections import namedtuple
 import torch
 import torch.nn as nn
-from pytorch_transformers import (WEIGHTS_NAME,XLNetConfig,XLNetForSequenceClassification)
+from pytorch_transformers import (XLNetConfig,XLNetForSequenceClassification)
 from tqdm import tqdm
 from torch.utils.data import (
     DataLoader,
     RandomSampler,
-    SequentialSampler,
     TensorDataset,
 )
 from pytorch_transformers import AdamW, WarmupLinearSchedule
 from utils_nlp.common.pytorch_utils import get_device, move_to_device
 from utils_nlp.models.xlnet.common import Language
-import random
 import mlflow
 import mlflow.pytorch
 import os
@@ -35,7 +33,7 @@ class XLNetSequenceClassifier:
                  max_grad_norm=1.0
                 ):
         """Initializes the classifier and the underlying pretrained model.
-        
+
         Args:
             language (Language, optional): The pretrained model's language.
                                            Defaults to 'xlnet-base-cased'.
@@ -52,12 +50,12 @@ class XLNetSequenceClassifier:
             lr (float): Learning rate of the Adam optimizer. Defaults to 5e-5.
             adam_eps (float, optional): term added to the denominator to improve
                                         numerical stability. Defaults to 1e-8.
-            warmup_steps (int, optional): Number of steps in which to increase 
+            warmup_steps (int, optional): Number of steps in which to increase
                                         learning rate linearly from 0 to 1. Defaults to 0.
             weight_decay (float, optional): Weight decay. Defaults to 0.
             max_grad_norm (float, optional): Maximum norm for the gradients. Defaults to 1.0
         """
-        
+
         if num_labels < 2:
             raise ValueError("Number of labels should be at least 2.")
         
@@ -75,9 +73,10 @@ class XLNetSequenceClassifier:
         self.max_grad_norm = max_grad_norm
         
         #create classifier
-        self.config = XLNetConfig.from_pretrained(self.language.value, num_labels=num_labels, cache_dir=cache_dir)
+        self.config = XLNetConfig.from_pretrained(self.language.value, num_labels=num_labels,\
+         cache_dir=cache_dir)
         self.model = XLNetForSequenceClassification(self.config)
-        
+
     def fit(
         self,
         token_ids,
@@ -94,7 +93,7 @@ class XLNetSequenceClassifier:
         val_steps = 0,
     ):
         """Fine-tunes the XLNet classifier using the given training data.
-        
+
         Args:
             token_ids (list): List of training token id lists.
             input_mask (list): List of input mask lists.
@@ -106,7 +105,7 @@ class XLNetSequenceClassifier:
             verbose (bool, optional): If True, shows the training progress and
                 loss values. Defaults to True.
         """
-        
+
         device = get_device("cpu" if self.num_gpus == 0 or not torch.cuda.is_available() else "gpu")
         self.model = move_to_device(self.model, device, self.num_gpus)
         
@@ -117,8 +116,6 @@ class XLNetSequenceClassifier:
         val_token_ids_tensor = torch.tensor(val_token_ids, dtype=torch.long)
         val_input_mask_tensor = torch.tensor(val_input_mask, dtype=torch.long)
         val_labels_tensor = torch.tensor(val_labels, dtype=torch.long)
-        
-        val_size = len(val_token_ids)
         
         if token_type_ids:
             token_type_ids_tensor = torch.tensor(token_type_ids, dtype=torch.long)
@@ -173,27 +170,22 @@ class XLNetSequenceClassifier:
             }
         ]
         
-        train_sampler = RandomSampler(train_dataset)
+
         val_sampler = RandomSampler(val_dataset)
-        
-        train_dataloader = DataLoader(
-            train_dataset,
-            sampler=train_sampler,
-            batch_size=self.batch_size
-        )
         
         val_dataloader = DataLoader(
             val_dataset,
             sampler=val_sampler,
             batch_size=self.batch_size
         )
-        
+
         num_examples = len(token_ids)
-        num_batches = len(train_dataloader)
+        num_batches = int(np.ceil(num_examples/self.batch_size))
         num_train_optimization_steps = num_batches * self.num_epochs
         
         optimizer = AdamW(optimizer_grouped_parameters, lr=self.lr, eps=self.adam_eps)
-        scheduler = WarmupLinearSchedule(optimizer, warmup_steps=self.warmup_steps, t_total=num_train_optimization_steps)
+        scheduler = WarmupLinearSchedule(optimizer, warmup_steps=self.warmup_steps,\
+         t_total=num_train_optimization_steps)
         
         global_step =0
         self.model.train()
@@ -207,7 +199,7 @@ class XLNetSequenceClassifier:
                 sampler=train_sampler,
                 batch_size=self.batch_size
             )
-            
+
             tr_loss = 0.0
             logging_loss = 0.0
             val_loss = 0.0
@@ -237,15 +229,15 @@ class XLNetSequenceClassifier:
 
                 tr_loss += loss.sum().item()
                 optimizer.step()
-                scheduler.step()  # Update learning rate schedule
-
+                # Update learning rate schedule
+                scheduler.step()
                 optimizer.zero_grad()
-
                 global_step += 1
                 # logging of learning rate and loss
                 if logging_steps > 0 and global_step % logging_steps == 0:
                     mlflow.log_metric("learning rate",scheduler.get_lr()[0],step=global_step)
-                    mlflow.log_metric("training loss",(tr_loss - logging_loss)/(logging_steps*self.batch_size),step=global_step)
+                    log_val = (tr_loss - logging_loss)/(logging_steps*self.batch_size)
+                    mlflow.log_metric("training loss", log_val, step=global_step)
                     logging_loss = tr_loss  
                 # model checkpointing    
                 if save_steps > 0 and global_step % save_steps == 0:
@@ -262,7 +254,8 @@ class XLNetSequenceClassifier:
                     val_loss = 0.0
                     for j, val_batch in enumerate(val_dataloader):
                         if token_type_ids:
-                            val_x_batch, val_mask_batch, val_token_type_ids_batch, val_y_batch = tuple(
+                            val_x_batch, val_mask_batch, val_token_type_ids_batch, \
+                            val_y_batch = tuple(
                                 t.to(device) for t in val_batch
                             )
                         else:
@@ -285,7 +278,8 @@ class XLNetSequenceClassifier:
                     if i % ((num_batches // 10) + 1) == 0:
                         if val_loss>0:    
                             print(
-                                "epoch:{}/{}; batch:{}->{}/{}; average training loss:{:.6f}; average val loss:{:.6f}".format(
+                                "epoch:{}/{}; batch:{}->{}/{}; average training loss:{:.6f};\
+                                 average val loss:{:.6f}".format(
                                     epoch + 1,
                                     self.num_epochs,
                                     i + 1,
@@ -297,7 +291,7 @@ class XLNetSequenceClassifier:
                             )
                         else:
                             print(
-                                "epoch:{}/{}; batch:{}->{}/{}; average training loss:{:.6f}".format(
+                                "epoch:{}/{}; batch:{}->{}/{}; average train loss:{:.6f}".format(
                                     epoch + 1,
                                     self.num_epochs,
                                     i + 1,
