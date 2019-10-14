e29036d2a048dea6990e15123aa9bd78406ceac5
saidbleik <saidbleik@outlook.com>
2019-10-14 20:52:43 +0000

    ignite demo

---
+++ examples/text_classification/tc_transformers_azureml_pipelines/scripts/train.py
@@ -0,0 +1,36 @@
+import os
+import pickle
+import shutil
+import sys
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+from utils_nlp.models.transformers.sequence_classification import Processor, SequenceClassifier
+import torch
+
+print("CUDA is{} available".format("" if torch.cuda.is_available() else " not"))
+
+input_dir = sys.argv[1]
+output_dir = sys.argv[2]
+model_name = sys.argv[3]
+num_gpus = int(sys.argv[4])
+cache_dir = "."
+num_labels = 5
+batch_size = 16
+
+if output_dir is not None:
+    os.makedirs(output_dir, exist_ok=True)
+
+# load dataset
+ds = pickle.load(open(os.path.join(input_dir, model_name + "_ds"), "rb"))
+# fine-tune
+classifier = SequenceClassifier(model_name=model_name, num_labels=num_labels, cache_dir=cache_dir)
+classifier.fit(ds, batch_size=batch_size, num_gpus=num_gpus, verbose=True)
+# write classifier
+pickle.dump(classifier, open(os.path.join(output_dir, model_name + "_clf"), "wb"))
+# write label encoder
+shutil.move(
+    os.path.join(input_dir, model_name + "_le"), os.path.join(output_dir, model_name + "_le")
+)
+
