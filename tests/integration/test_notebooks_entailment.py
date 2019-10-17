4b6295b6540a51fb712cbf7a1e87218e37123d5b
hlums <honglu@microsoft.com>
2019-10-17 17:34:39 +0000

    Add tmp path.

--- tests/integration/test_notebooks_entailment.py
+++ tests/integration/test_notebooks_entailment.py
@@ -10,9 +10,10 @@ from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
 
 ABS_TOL = 0.1
 
+
 @pytest.mark.gpu
 @pytest.mark.integration
-def test_entailment_multinli_bert(notebooks):
+def test_entailment_multinli_bert(notebooks, tmp):
     notebook_path = notebooks["entailment_multinli_bert"]
     pm.execute_notebook(
         notebook_path,
@@ -21,6 +22,7 @@ def test_entailment_multinli_bert(notebooks):
             "TRAIN_DATA_USED_PERCENT": 0.001,
             "DEV_DATA_USED_PERCENT": 0.01,
             "NUM_EPOCHS": 1,
+            "CACHE_DIR": tmp
         },
         kernel_name=KERNEL_NAME,
     )
