269b26eb0281bd57712b17c3a865035e1107eef2
saidbleik <saidbleik@outlook.com>
2019-10-24 20:22:09 +0000

    update data  utils

--- utils_nlp/dataset/multinli.py
+++ utils_nlp/dataset/multinli.py
@@ -20,41 +20,21 @@ DATA_FILES = {
     "dev_matched": "multinli_1.0/multinli_1.0_dev_matched.jsonl",
     "dev_mismatched": "multinli_1.0/multinli_1.0_dev_mismatched.jsonl",
 }
+SAMPLE_URL = "https://nlpbp.blob.core.windows.net/data/mnli_sample.csv"
 
 
-def download_file_and_extract(local_cache_path: str = ".", file_split: str = "train") -> None:
-    """Download and extract the dataset files
-
-    Args:
-        local_cache_path (str [optional]) -- Directory to cache files to. Defaults to current working directory (default: {"."})
-        file_split {str} -- [description] (default: {"train"})
-    
-    Returns:
-        None -- Nothing is returned
-    """
-    file_name = URL.split("/")[-1]
-    maybe_download(URL, file_name, local_cache_path)
-
-    if not os.path.exists(os.path.join(local_cache_path, DATA_FILES[file_split])):
-        extract_zip(os.path.join(local_cache_path, file_name), local_cache_path)
+def download_file_and_extract(local_cache_path: str = ".") -> None:
+    file_name = SAMPLE_URL.split("/")[-1]
+    maybe_download(SAMPLE_URL, file_name, local_cache_path)
+    return file_name
 
 
-def load_pandas_df(local_cache_path=".", file_split="train"):
-    """Loads extracted dataset into pandas
-    Args:
-        local_cache_path ([type], optional): [description]. Defaults to current working directory.
-        file_split (str, optional): The subset to load.
-            One of: {"train", "dev_matched", "dev_mismatched"}
-            Defaults to "train".
-    Returns:
-        pd.DataFrame: pandas DataFrame containing the specified
-            MultiNLI subset.
-    """
+def load_pandas_df(local_cache_path="."):
     try:
-        download_file_and_extract(local_cache_path, file_split)
+        file_name = download_file_and_extract(local_cache_path)        
     except Exception as e:
         raise e
-    return pd.read_json(os.path.join(local_cache_path, DATA_FILES[file_split]), lines=True)
+    return pd.read_csv(os.path.join(local_cache_path, file_name))
 
 
 def get_generator(
