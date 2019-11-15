dab0f019c8a016d23590c8f122173c552e7cd666
hlums <honglu@microsoft.com>
2019-11-15 03:13:03 +0000

    Update entailment notebook test.

--- tests/integration/test_notebooks_entailment.py
+++ tests/integration/test_notebooks_entailment.py
@@ -15,13 +15,15 @@ ABS_TOL = 0.1
 @pytest.mark.gpu
 @pytest.mark.integration
 def test_entailment_multinli_bert(notebooks, tmp):
-    notebook_path = notebooks["entailment_multinli_bert"]
+    notebook_path = notebooks["entailment_multinli_transformers"]
     pm.execute_notebook(
         notebook_path,
         OUTPUT_NOTEBOOK,
         parameters={
-            "TRAIN_DATA_USED_PERCENT": 0.05,
-            "DEV_DATA_USED_PERCENT": 0.05,
+            "MODEL_NAME": "bert-base-uncased",
+            "TO_LOWER": True,
+            "TRAIN_DATA_USED_FRACTION": 0.05,
+            "DEV_DATA_USED_FRACTION": 0.05,
             "NUM_EPOCHS": 1,
             "CACHE_DIR": tmp
         },
@@ -35,29 +37,29 @@ def test_entailment_multinli_bert(notebooks, tmp):
     assert pytest.approx(result["mismatched_recall"], 0.76, abs=ABS_TOL)
     assert pytest.approx(result["mismatched_f1"], 0.76, abs=ABS_TOL)
 
-@pytest.mark.integration
-@pytest.mark.azureml
-def test_entailment_xnli_bert_azureml(
-    notebooks, subscription_id, resource_group, workspace_name, workspace_region, cluster_name
-):
-    notebook_path = notebooks["entailment_xnli_bert_azureml"]
-    pm.execute_notebook(
-        notebook_path,
-        OUTPUT_NOTEBOOK,
-        parameters={
-            "DATA_PERCENT_USED": 0.0025,
-            "subscription_id": subscription_id,
-            "resource_group": resource_group,
-            "workspace_name": workspace_name,
-            "workspace_region": workspace_region,
-            "cluster_name": cluster_name,
-        },
-        kernel_name=KERNEL_NAME,
-    )
+# @pytest.mark.integration
+# @pytest.mark.azureml
+# def test_entailment_xnli_bert_azureml(
+#     notebooks, subscription_id, resource_group, workspace_name, workspace_region, cluster_name
+# ):
+#     notebook_path = notebooks["entailment_xnli_bert_azureml"]
+#     pm.execute_notebook(
+#         notebook_path,
+#         OUTPUT_NOTEBOOK,
+#         parameters={
+#             "DATA_PERCENT_USED": 0.0025,
+#             "subscription_id": subscription_id,
+#             "resource_group": resource_group,
+#             "workspace_name": workspace_name,
+#             "workspace_region": workspace_region,
+#             "cluster_name": cluster_name,
+#         },
+#         kernel_name=KERNEL_NAME,
+#     )
 
-    with open("outputs/results.json", "r") as handle:
-        result_dict = json.load(handle)
-        assert result_dict["weighted avg"]["f1-score"] == pytest.approx(0.2, abs=ABS_TOL)
+#     with open("outputs/results.json", "r") as handle:
+#         result_dict = json.load(handle)
+#         assert result_dict["weighted avg"]["f1-score"] == pytest.approx(0.2, abs=ABS_TOL)
 
-    if os.path.exists("outputs"):
-        shutil.rmtree("outputs")
+#     if os.path.exists("outputs"):
+#         shutil.rmtree("outputs")
