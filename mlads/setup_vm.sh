f6de70e46b9abdeac97b634504047b5a34622575
Emmanuel Awa <emmanuel.awa@microsoft.com>
2019-11-18 03:59:53 +0000

    setup script to run commands on vms

---
+++ mlads/setup_vm.sh
@@ -0,0 +1,42 @@
+cd ~  
+
+# clone repo and install the conda env 
+git clone https://github.com/microsoft/nlp-recipes.git
+
+# change permission as we copy this into each user's folder
+chmod -R ugo+rwx /root/nlp-recipes
+
+source /data/anaconda/etc/profile.d/conda.sh
+
+cd /root/nlp-recipes
+python tools/generate_conda_file.py
+conda env create -n nlp_cpu -f nlp_cpu.yaml
+
+conda activate my_env_name
+python -m ipykernel install --user --name nlp_cpu --display-name "Python (nlp_cpu)"
+
+# Pip install the utils_nlp package in the conda environment
+pip install -e . 
+
+
+# add users to jupyterhub
+echo 'c.Authenticator.whitelist = {"mlads1", "mlads2", "mlads3", "mlads4", "mlads5", "mlads6", "mlads7", "mlads8", "mlads9", "mlads10", "mlads11", "mlads12", "mlads13", "mlads14", "mlads15", "mlads16", "mlads17", "mlads18", "mlads19", "mlads20", "mlads21", "mlads22", "mlads23", "mlads24", "mlads25", "mlads26", "mlads27", "mlads28", "mlads29", "mlads30", "mlads31", "mlads32"}' | sudo tee -a /etc/jupyterhub/jupyterhub_config.py
+
+# create the users on the vm 
+for i in {1..32}
+do
+    USERNAME=mlads$i
+    PASSWORD=nlpmlads$i
+    sudo adduser --quiet --disabled-password --gecos "" $USERNAME
+    echo "$USERNAME:$PASSWORD" | sudo chpasswd
+    rm -rf /data/home/$USERNAME/notebooks/*
+    
+    # copy repo
+    cp -ar /root/nlp-recipes /data/home/$USERNAME/notebooks
+done
+
+# restart jupyterhub 
+sudo systemctl stop jupyterhub 
+sudo systemctl start jupyterhub 
+
+exit
