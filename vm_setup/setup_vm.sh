582bf0e35215ffe580d996b97ef72765ee341ec8
Emmanuel Awa <emmanuel.awa@microsoft.com>
2019-11-20 00:03:39 +0000

    Add only 15 users per jupyterhub

--- vm_setup/setup_vm.sh
+++ vm_setup/setup_vm.sh
@@ -20,10 +20,10 @@ pip install -e .
 
 
 # add users to jupyterhub
-echo 'c.Authenticator.whitelist = {"mlads1", "mlads2", "mlads3", "mlads4", "mlads5", "mlads6", "mlads7", "mlads8", "mlads9", "mlads10", "mlads11", "mlads12", "mlads13", "mlads14", "mlads15", "mlads16", "mlads17", "mlads18", "mlads19", "mlads20", "mlads21", "mlads22", "mlads23", "mlads24", "mlads25", "mlads26", "mlads27", "mlads28", "mlads29", "mlads30", "mlads31", "mlads32"}' | sudo tee -a /etc/jupyterhub/jupyterhub_config.py
+echo 'c.Authenticator.whitelist = {"mlads1", "mlads2", "mlads3", "mlads4", "mlads5", "mlads6", "mlads7", "mlads8", "mlads9", "mlads10", "mlads11", "mlads12", "mlads13", "mlads14", "mlads15"}' | sudo tee -a /etc/jupyterhub/jupyterhub_config.py
 
 # create the users on the vm 
-for i in {1..32}
+for i in {1..15}
 do
     USERNAME=mlads$i
     PASSWORD=nlpmlads$i
