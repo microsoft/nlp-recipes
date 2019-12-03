75e6eb9c95126d16b35fbcb1b1e487ae2e0a6eeb
Emmanuel Awa <emmanuel.awa@microsoft.com>
2019-12-03 15:13:27 +0000

    update: major release version to 2.0.0

--- utils_nlp/__init__.py
+++ utils_nlp/__init__.py
@@ -5,7 +5,7 @@ __title__ = "Microsoft NLP"
 __author__ = "AI CAT at Microsoft"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018-present Microsoft Corporation"
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 
 # Synonyms
 TITLE = __title__
