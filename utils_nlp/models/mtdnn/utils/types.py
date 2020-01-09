1059fd0bc37a3d4cf9b19a4aa576e8dedce3802e
Emmanuel Awa <emmanuel.awa@microsoft.com>
2020-01-09 18:50:16 +0000

    modif: remove xlnet and spell check

--- utils_nlp/models/mtdnn/utils/types.py
+++ utils_nlp/models/mtdnn/utils/types.py
@@ -1,22 +1,23 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 from enum import IntEnum
+
+
 class TaskType(IntEnum):
     Classification = 1
     Regression = 2
     Ranking = 3
     Span = 4
-    SeqenceLabeling = 5
-
+    SequenceLabeling = 5
 
 
 class DataFormat(IntEnum):
     PremiseOnly = 1
     PremiseAndOneHypothesis = 2
     PremiseAndMultiHypothesis = 3
-    Seqence = 4
+    Sequence = 4
+
 
 class EncoderModelType(IntEnum):
     BERT = 1
     ROBERTA = 2
-    XLNET = 3
