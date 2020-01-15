b5fd983ae4025f5b7db620f621f298cf64c4cf66
Emmanuel Awa <emmanuel.awa@microsoft.com>
2020-01-15 15:39:46 +0000

    wip

--- utils_nlp/models/mtdnn/modeling_mtdnn.py
+++ utils_nlp/models/mtdnn/modeling_mtdnn.py
@@ -21,11 +21,13 @@ from transformers import (
 
 from fairseq.models.roberta import RobertaModel as FairseqRobertModel
 from utils_nlp.dataset.url_utils import maybe_download
+from utils_nlp.models.mtdnn.common.archive_maps import PRETRAINED_MODEL_ARCHIVE_MAP
 from utils_nlp.models.mtdnn.common.average_meter import AverageMeter
 from utils_nlp.models.mtdnn.common.linear_pooler import LinearPooler
+from utils_nlp.models.mtdnn.common.san import SANClassifier, SANNetwork
 from utils_nlp.models.mtdnn.common.types import DataFormat, EncoderModelType, TaskType
 from utils_nlp.models.mtdnn.configuration_mtdnn import MTDNNConfig
-from utils_nlp.models.mtdnn.common.archive_maps import PRETRAINED_MODEL_ARCHIVE_MAP
+
 
 logger = logging.getLogger(__name__)
 
@@ -51,127 +53,311 @@ class MTDNNPretrainedModel(BertPreTrainedModel):
 
 
 class MTDNNModel(MTDNNPretrainedModel, BertModel):
-    def __init__(self, config: MTDNNConfig):
+    def __init__(self, config: MTDNNConfig, num_train_step: int = -1):
         super(MTDNNModel, self).__init__(config)
         self.config = config
-        self.local_updates = 0
-        self.train_loss = AverageMeter()
-        self.dropout_list = nn.ModuleList()
-        self.encoder_type = config.encoder_type
         self.config_dict = self.config.to_dict()
         self.mtdnn_config = MTDNNConfig.from_dict(self.config_dict)
+        self.mtdnn_model = self.from_pretrained("mtdnn-base-uncased")
+        self.state_dict = self.mtdnn_model.state_dict()
+        self.updates = (
+            self.state_dict["updates"] if self.state_dict and "updates" in self.state_dict else 0
+        )
+        self.local_updates = 0
+        self.train_loss = AverageMeter()
+        self.network = SANNetwork(self.config)
+        if self.state_dict:
+            self.network.load_state_dict(self.state_dict["state"], strict=False)
+        self.mnetwork = nn.DataParallel(self.network) if self.config.multi_gpu_on else self.network
+        self.total_param = sum([p.nelement() for p in self.network.parameters() if p.requires_grad])
 
-        # Setup the baseline model
-        # Define the encoder based on config options
-        self.bert_config = BertConfig.from_dict(self.mtdnn_config)
-        self.bert = BertModel(self.bert_config)
-        self.hidden_size = self.bert_config.hidden_size
-        if self.encoder_type == EncoderModelType.ROBERTA:
-            self.bert = FairseqRobertModel.from_pretrained(config.init_checkpoint)
-            self.hidden_size = self.bert.args.encoder_embed_dim
-            self.pooler = LinearPooler(hidden_size)
+        if self.config.cuda:
+            self.network.cuda()
+        self.optimizer_parameters = self._get_param_groups()
+        self._setup_optim(self.optimizer_parameters, self.state_dict, num_train_step)
+        self.para_swapped = False
+        self.optimizer.zero_grad()
+        self._setup_lossmap(self.config)
 
-        # Dump other features if value is set to true
-        if config.dump_feature:
-            return
+    def _get_param_groups(self):
+        no_decay = ["bias", "gamma", "beta", "LayerNorm.bias", "LayerNorm.weight"]
+        optimizer_parameters = [
+            {
+                "params": [
+                    p
+                    for n, p in self.network.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": 0.01,
+            },
+            {
+                "params": [
+                    p for n, p in self.network.named_parameters() if any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": 0.0,
+            },
+        ]
+        return optimizer_parameters
 
-        # Set decoder and scoring list parameters
-        self.decoder_opts = config.decoder_opts
-        self.scoring_list = nn.ModuleList()
-
-        # Update bert parameters
-        if config.update_bert_opt > 0:
-            for param in self.bert.parameters():
-                param.requires_grad = False
-
-        # Set task specific paramaters
-        self.task_types = config.task_types
-        self.task_dropout_p = config.tasks_dropout_p
-        self.n_class = config.n_class
-        for task, label in enumerate(self.n_class):
-            task_type = self.task_types[task]
-            dropout = DropoutWrapper(
-                self.task_dropout_p[task], config["enable_variational_dropout"]
+    def _setup_optim(self, optimizer_parameters, state_dict=None, num_train_step=-1):
+        if self.config.optimizer == "sgd":
+            self.optimizer = optim.SGD(
+                optimizer_parameters,
+                self.config["learning_rate"],
+                weight_decay=self.config["weight_decay"],
             )
-            self.dropout_list.append(dropout)
-            if task_type == TaskType.Span:
-                out_proj = nn.Linear(hidden_size, 2)
-            elif task_type == TaskType.SequenceLabeling:
-                out_proj = nn.Linear(hidden_size, label)
-            else:
-                out_proj = nn.Linear(hidden_size, label)
-            self.scoring_list.append(out_proj)
 
-        self._my_init()
-
-    def init_encoder(self, encoder_type: int = 1):
-        """ Set the model encoder during initialization
-        encoder_type set to 1 means BERT, 2 means RoBERTa
-        """
-        pass
-
-    def _my_init(self):
-        def init_weights(module):
-            if isinstance(module, (nn.Linear, nn.Embedding)):
-                # Slightly different from the TF version which uses truncated_normal for initialization
-                # cf https://github.com/pytorch/pytorch/pull/5617
-                module.weight.data.normal_(mean=0.0, std=0.02 * self.opt["init_ratio"])
-            elif isinstance(module, BertLayerNorm):
-                # Slightly different from the BERT pytorch version, which should be a bug.
-                # Note that it only affects on training from scratch. For detailed discussions, please contact xiaodl@.
-                # Layer normalization (https://arxiv.org/abs/1607.06450)
-                # support both old/latest version
-                if "beta" in dir(module) and "gamma" in dir(module):
-                    module.beta.data.zero_()
-                    module.gamma.data.fill_(1.0)
-                else:
-                    module.bias.data.zero_()
-                    module.weight.data.fill_(1.0)
-            if isinstance(module, nn.Linear):
-                module.bias.data.zero_()
-
-        self.apply(init_weights)
-
-    def forward(
-        self, input_ids, token_type_ids, attention_mask, premise_mask=None, hyp_mask=None, task_id=0
-    ):
-        if self.encoder_type == EncoderModelType.ROBERTA:
-            sequence_output = self.bert.extract_features(input_ids)
-            pooled_output = self.pooler(sequence_output)
+        elif self.config.optimizer == "adamax":
+            self.optimizer = Adamax(
+                optimizer_parameters,
+                self.config.learning_rate,
+                warmup=self.config.warmup,
+                t_total=num_train_step,
+                max_grad_norm=self.config.grad_clipping,
+                schedule=self.config.warmup_schedule,
+                weight_decay=self.config.weight_decay,
+            )
+            if self.config.get("have_lr_scheduler", False):
+                self.config["have_lr_scheduler"] = False
+        elif self.config["optimizer"] == "radam":
+            self.optimizer = RAdam(
+                optimizer_parameters,
+                self.config["learning_rate"],
+                warmup=self.config["warmup"],
+                t_total=num_train_step,
+                max_grad_norm=self.config["grad_clipping"],
+                schedule=self.config["warmup_schedule"],
+                eps=self.config["adam_eps"],
+                weight_decay=self.config["weight_decay"],
+            )
+            if self.config.get("have_lr_scheduler", False):
+                self.config["have_lr_scheduler"] = False
+            # The current radam does not support FP16.
+            self.config["fp16"] = False
+        elif self.config["optimizer"] == "adam":
+            self.optimizer = Adam(
+                optimizer_parameters,
+                lr=self.config["learning_rate"],
+                warmup=self.config["warmup"],
+                t_total=num_train_step,
+                max_grad_norm=self.config["grad_clipping"],
+                schedule=self.config["warmup_schedule"],
+                weight_decay=self.config["weight_decay"],
+            )
+            if self.config.get("have_lr_scheduler", False):
+                self.config["have_lr_scheduler"] = False
         else:
-            all_encoder_layers, pooled_output = self.bert(input_ids, token_type_ids, attention_mask)
-            sequence_output = all_encoder_layers[-1]
+            raise RuntimeError("Unsupported optimizer: %s" % opt["optimizer"])
 
-        decoder_opt = self.decoder_opts[task_id]
-        task_type = self.task_types[task_id]
-        if task_type == TaskType.Span:
-            assert decoder_opt != 1
-            sequence_output = self.dropout_list[task_id](sequence_output)
-            logits = self.scoring_list[task_id](sequence_output)
-            start_scores, end_scores = logits.split(1, dim=-1)
-            start_scores = start_scores.squeeze(-1)
-            end_scores = end_scores.squeeze(-1)
-            return start_scores, end_scores
-        elif task_type == TaskType.SequenceLabeling:
-            pooled_output = all_encoder_layers[-1]
-            pooled_output = self.dropout_list[task_id](pooled_output)
-            pooled_output = pooled_output.contiguous().view(-1, pooled_output.size(2))
-            logits = self.scoring_list[task_id](pooled_output)
-            return logits
-        else:
-            if decoder_opt == 1:
-                max_query = hyp_mask.size(1)
-                assert max_query > 0
-                assert premise_mask is not None
-                assert hyp_mask is not None
-                hyp_mem = sequence_output[:, :max_query, :]
-                logits = self.scoring_list[task_id](
-                    sequence_output, hyp_mem, premise_mask, hyp_mask
+        if state_dict and "optimizer" in state_dict:
+            self.optimizer.load_state_dict(state_dict["optimizer"])
+
+        if self.config["fp16"]:
+            try:
+                from apex import amp
+
+                global amp
+            except ImportError:
+                raise ImportError(
+                    "Please install apex from https://www.github.com/nvidia/apex to use fp16 training."
+                )
+            model, optimizer = amp.initialize(
+                self.network, self.optimizer, opt_level=self.config["fp16_opt_level"]
+            )
+            self.network = model
+            self.optimizer = optimizer
+
+        if self.config.get("have_lr_scheduler", False):
+            if self.config.get("scheduler_type", "rop") == "rop":
+                self.scheduler = ReduceLROnPlateau(
+                    self.optimizer, mode="max", factor=self.config["lr_gamma"], patience=3
+                )
+            elif self.config.get("scheduler_type", "rop") == "exp":
+                self.scheduler = ExponentialLR(
+                    self.optimizer, gamma=self.config.get("lr_gamma", 0.95)
                 )
             else:
-                pooled_output = self.dropout_list[task_id](pooled_output)
-                logits = self.scoring_list[task_id](pooled_output)
-            return logits
+                milestones = [
+                    int(step) for step in self.config.get("multi_step_lr", "10,20,30").split(",")
+                ]
+                self.scheduler = MultiStepLR(
+                    self.optimizer, milestones=milestones, gamma=self.config.get("lr_gamma")
+                )
+        else:
+            self.scheduler = None
+
+    def _setup_lossmap(self, config):
+        loss_types = config["loss_types"]
+        self.task_loss_criterion = []
+        for idx, cs in enumerate(loss_types):
+            assert cs is not None
+            lc = LOSS_REGISTRY[cs](name="Loss func of task {}: {}".format(idx, cs))
+            self.task_loss_criterion.append(lc)
+
+    def _setup_kd_lossmap(self, config):
+        loss_types = config["kd_loss_types"]
+        self.kd_task_loss_criterion = []
+        if config.get("mkd_opt", 0) > 0:
+            for idx, cs in enumerate(loss_types):
+                assert cs is not None
+                lc = LOSS_REGISTRY[cs](name="Loss func of task {}: {}".format(idx, cs))
+                self.kd_task_loss_criterion.append(lc)
+
+    def train(self):
+        if self.para_swapped:
+            self.para_swapped = False
+
+    def _to_cuda(self, tensor):
+        if tensor is None:
+            return tensor
+
+        if isinstance(tensor, list) or isinstance(tensor, tuple):
+            y = [e.cuda(non_blocking=True) for e in tensor]
+            for e in y:
+                e.requires_grad = False
+        else:
+            y = tensor.cuda(non_blocking=True)
+            y.requires_grad = False
+        return y
+
+    def update(self, batch_meta, batch_data):
+        self.network.train()
+        y = batch_data[batch_meta["label"]]
+        soft_labels = None
+
+        task_type = batch_meta["task_type"]
+        y = self._to_cuda(y) if self.config["cuda"] else y
+
+        task_id = batch_meta["task_id"]
+        inputs = batch_data[: batch_meta["input_len"]]
+        if len(inputs) == 3:
+            inputs.append(None)
+            inputs.append(None)
+        inputs.append(task_id)
+        weight = None
+        if self.config.get("weighted_on", False):
+            if self.config["cuda"]:
+                weight = batch_data[batch_meta["factor"]].cuda(non_blocking=True)
+            else:
+                weight = batch_data[batch_meta["factor"]]
+        logits = self.mnetwork(*inputs)
+
+        # compute loss
+        loss = 0
+        if self.task_loss_criterion[task_id] and (y is not None):
+            loss = self.task_loss_criterion[task_id](logits, y, weight, ignore_index=-1)
+
+        # compute kd loss
+        if self.config.get("mkd_opt", 0) > 0 and ("soft_label" in batch_meta):
+            soft_labels = batch_meta["soft_label"]
+            soft_labels = self._to_cuda(soft_labels) if self.config["cuda"] else soft_labels
+            kd_lc = self.kd_task_loss_criterion[task_id]
+            kd_loss = kd_lc(logits, soft_labels, weight, ignore_index=-1) if kd_lc else 0
+            loss = loss + kd_loss
+
+        self.train_loss.update(loss.item(), batch_data[batch_meta["token_id"]].size(0))
+        # scale loss
+        loss = loss / self.config.get("grad_accumulation_step", 1)
+        if self.config["fp16"]:
+            with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
+        self.local_updates += 1
+        if self.local_updates % self.config.get("grad_accumulation_step", 1) == 0:
+            if self.config["global_grad_clipping"] > 0:
+                if self.config["fp16"]:
+                    torch.nn.utils.clip_grad_norm_(
+                        amp.master_params(self.optimizer), self.config["global_grad_clipping"]
+                    )
+                else:
+                    torch.nn.utils.clip_grad_norm_(
+                        self.network.parameters(), self.config["global_grad_clipping"]
+                    )
+            self.updates += 1
+            # reset number of the grad accumulation
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+    def predict(self, batch_meta, batch_data):
+        self.network.eval()
+        task_id = batch_meta["task_id"]
+        task_type = batch_meta["task_type"]
+        inputs = batch_data[: batch_meta["input_len"]]
+        if len(inputs) == 3:
+            inputs.append(None)
+            inputs.append(None)
+        inputs.append(task_id)
+        score = self.mnetwork(*inputs)
+        if task_type == TaskType.Ranking:
+            score = score.contiguous().view(-1, batch_meta["pairwise_size"])
+            assert task_type == TaskType.Ranking
+            score = F.softmax(score, dim=1)
+            score = score.data.cpu()
+            score = score.numpy()
+            predict = np.zeros(score.shape, dtype=int)
+            positive = np.argmax(score, axis=1)
+            for idx, pos in enumerate(positive):
+                predict[idx, pos] = 1
+            predict = predict.reshape(-1).tolist()
+            score = score.reshape(-1).tolist()
+            return score, predict, batch_meta["true_label"]
+        elif task_type == TaskType.SeqenceLabeling:
+            mask = batch_data[batch_meta["mask"]]
+            score = score.contiguous()
+            score = score.data.cpu()
+            score = score.numpy()
+            predict = np.argmax(score, axis=1).reshape(mask.size()).tolist()
+            valied_lenght = mask.sum(1).tolist()
+            final_predict = []
+            for idx, p in enumerate(predict):
+                final_predict.append(p[: valied_lenght[idx]])
+            score = score.reshape(-1).tolist()
+            return score, final_predict, batch_meta["label"]
+        elif task_type == TaskType.Span:
+            start, end = score
+            predictions = []
+            if self.config["encoder_type"] == EncoderModelType.BERT:
+                import experiments.squad.squad_utils as mrc_utils
+
+                scores, predictions = mrc_utils.extract_answer(
+                    batch_meta, batch_data, start, end, self.config.get("max_answer_len", 5)
+                )
+            return scores, predictions, batch_meta["answer"]
+        else:
+            if task_type == TaskType.Classification:
+                score = F.softmax(score, dim=1)
+            score = score.data.cpu()
+            score = score.numpy()
+            predict = np.argmax(score, axis=1).tolist()
+            score = score.reshape(-1).tolist()
+        return score, predict, batch_meta["label"]
+
+    def extract(self, batch_meta, batch_data):
+        self.network.eval()
+        # 'token_id': 0; 'segment_id': 1; 'mask': 2
+        inputs = batch_data[:3]
+        all_encoder_layers, pooled_output = self.mnetwork.bert(*inputs)
+        return all_encoder_layers, pooled_output
+
+    def save(self, filename):
+        network_state = dict([(k, v.cpu()) for k, v in self.network.state_dict().items()])
+        params = {
+            "state": network_state,
+            "optimizer": self.optimizer.state_dict(),
+            "config": self.config,
+        }
+        torch.save(params, filename)
+        logger.info("model saved to {}".format(filename))
+
+    def load(self, checkpoint):
+        model_state_dict = torch.load(checkpoint)
+        self.network.load_state_dict(model_state_dict["state"], strict=False)
+        self.optimizer.load_state_dict(model_state_dict["optimizer"])
+        self.config.update(model_state_dict["config"])
+
+    def cuda(self):
+        self.network.cuda()
 
 
 if __name__ == "__main__":
