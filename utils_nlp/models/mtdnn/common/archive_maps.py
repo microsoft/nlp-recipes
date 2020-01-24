87b3e576cf6b1bb67a13916e0d790f0c3c5fdc65
Emmanuel Awa <emmanuel.awa@microsoft.com>
2020-01-24 20:58:46 +0000

    clean up

--- utils_nlp/models/mtdnn/common/archive_maps.py
+++ utils_nlp/models/mtdnn/common/archive_maps.py
@@ -1,45 +1,6 @@
 # coding=utf-8
 # Copyright (c) Microsoft. All rights reserved.
 
-from ..tasks.config import (
-    COLATaskConfig,
-    MNLITaskConfig,
-    MRPCTaskConfig,
-    QNLITaskConfig,
-    QQPTaskConfig,
-    RTETaskConfig,
-    SCITAILTaskConfig,
-    SNLITaskConfig,
-    SSTTaskConfig,
-    STSBTaskConfig,
-    WNLITaskConfig,
-    NERTaskConfig,
-    POSTaskConfig,
-    CHUNKTaskConfig,
-    SQUADTaskConfig,
-)
-
-# Map of supported tasks
-SUPPORTED_TASKS_MAP = {
-    "cola": COLATaskConfig,
-    "mnli": MNLITaskConfig,
-    "mrpc": MRPCTaskConfig,
-    "qnli": QNLITaskConfig,
-    "qqp": QQPTaskConfig,
-    "rte": RTETaskConfig,
-    "scitail": SCITAILTaskConfig,
-    "snli": SNLITaskConfig,
-    "sst": SSTTaskConfig,
-    "stsb": STSBTaskConfig,
-    "wnli": WNLITaskConfig,
-    "ner": NERTaskConfig,
-    "pos": POSTaskConfig,
-    "chunk": CHUNKTaskConfig,
-    "squad": SQUADTaskConfig,
-    "squad-v2": SQUADTaskConfig,
-}
-
-
 PRETRAINED_MODEL_ARCHIVE_MAP = {
     "mtdnn-base-uncased": "https://mrc.blob.core.windows.net/mt-dnn-model/mt_dnn_base.pt",
     "mtdnn-large-uncased": "https://mrc.blob.core.windows.net/mt-dnn-model/mt_dnn_large.pt",
