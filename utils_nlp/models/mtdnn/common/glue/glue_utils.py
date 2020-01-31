2b5b8cb9322f03af32f80d36c7085fa80d819be7
Emmanuel Awa <emmanuel.awa@microsoft.com>
2020-01-31 20:13:32 +0000

    refactor

--- utils_nlp/models/mtdnn/common/glue/glue_utils.py
+++ utils_nlp/models/mtdnn/common/glue/glue_utils.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 from random import shuffle
-from data_utils.metrics import calc_metrics
+
+from utils_nlp.models.mtdnn.common.metrics import calc_metrics
 
 
 def load_scitail(file):
@@ -10,13 +11,20 @@ def load_scitail(file):
     cnt = 0
     with open(file, encoding="utf8") as f:
         for line in f:
-            blocks = line.strip().split('\t')
+            blocks = line.strip().split("\t")
             assert len(blocks) > 2
-            if blocks[0] == '-': continue
-            sample = {'uid': str(cnt), 'premise': blocks[0], 'hypothesis': blocks[1], 'label': blocks[2]}
+            if blocks[0] == "-":
+                continue
+            sample = {
+                "uid": str(cnt),
+                "premise": blocks[0],
+                "hypothesis": blocks[1],
+                "label": blocks[2],
+            }
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_snli(file, header=True):
     rows = []
@@ -26,16 +34,20 @@ def load_snli(file, header=True):
             if header:
                 header = False
                 continue
-            blocks = line.strip().split('\t')
+            blocks = line.strip().split("\t")
             assert len(blocks) > 10
-            if blocks[-1] == '-': continue
+            if blocks[-1] == "-":
+                continue
             lab = blocks[-1]
             if lab is None:
-                import pdb; pdb.set_trace()
-            sample = {'uid': blocks[0], 'premise': blocks[7], 'hypothesis': blocks[8], 'label': lab}
+                import pdb
+
+                pdb.set_trace()
+            sample = {"uid": blocks[0], "premise": blocks[7], "hypothesis": blocks[8], "label": lab}
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_mnli(file, header=True, multi_snli=False, is_train=True):
     rows = []
@@ -45,18 +57,22 @@ def load_mnli(file, header=True, multi_snli=False, is_train=True):
             if header:
                 header = False
                 continue
-            blocks = line.strip().split('\t')
+            blocks = line.strip().split("\t")
             assert len(blocks) > 9
-            if blocks[-1] == '-': continue
+            if blocks[-1] == "-":
+                continue
             lab = "contradiction"
             if is_train:
                 lab = blocks[-1]
             if lab is None:
-                import pdb; pdb.set_trace()
-            sample = {'uid': blocks[0], 'premise': blocks[8], 'hypothesis': blocks[9], 'label': lab}
+                import pdb
+
+                pdb.set_trace()
+            sample = {"uid": blocks[0], "premise": blocks[8], "hypothesis": blocks[9], "label": lab}
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_mrpc(file, header=True, is_train=True):
     rows = []
@@ -66,15 +82,16 @@ def load_mrpc(file, header=True, is_train=True):
             if header:
                 header = False
                 continue
-            blocks = line.strip().split('\t')
+            blocks = line.strip().split("\t")
             assert len(blocks) > 4
             lab = 0
             if is_train:
                 lab = int(blocks[0])
-            sample = {'uid': cnt, 'premise': blocks[-2], 'hypothesis': blocks[-1], 'label': lab}
+            sample = {"uid": cnt, "premise": blocks[-2], "hypothesis": blocks[-1], "label": lab}
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_qnli(file, header=True, is_train=True):
     """QNLI for classification"""
@@ -85,17 +102,20 @@ def load_qnli(file, header=True, is_train=True):
             if header:
                 header = False
                 continue
-            blocks = line.strip().split('\t')
+            blocks = line.strip().split("\t")
             assert len(blocks) > 2
             lab = "not_entailment"
             if is_train:
                 lab = blocks[-1]
             if lab is None:
-                import pdb; pdb.set_trace()
-            sample = {'uid': blocks[0], 'premise': blocks[1], 'hypothesis': blocks[2], 'label': lab}
+                import pdb
+
+                pdb.set_trace()
+            sample = {"uid": blocks[0], "premise": blocks[1], "hypothesis": blocks[2], "label": lab}
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_qqp(file, header=True, is_train=True):
     rows = []
@@ -106,20 +126,27 @@ def load_qqp(file, header=True, is_train=True):
             if header:
                 header = False
                 continue
-            blocks = line.strip().split('\t')
+            blocks = line.strip().split("\t")
             if is_train and len(blocks) < 6:
                 skipped += 1
                 continue
-            if not is_train: assert len(blocks) == 3
+            if not is_train:
+                assert len(blocks) == 3
             lab = 0
             if is_train:
                 lab = int(blocks[-1])
-                sample = {'uid': cnt, 'premise': blocks[-3], 'hypothesis': blocks[-2], 'label': lab}
+                sample = {"uid": cnt, "premise": blocks[-3], "hypothesis": blocks[-2], "label": lab}
             else:
-                sample = {'uid': int(blocks[0]), 'premise': blocks[-2], 'hypothesis': blocks[-1], 'label': lab}
+                sample = {
+                    "uid": int(blocks[0]),
+                    "premise": blocks[-2],
+                    "hypothesis": blocks[-1],
+                    "label": lab,
+                }
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_rte(file, header=True, is_train=True):
     rows = []
@@ -127,20 +154,33 @@ def load_rte(file, header=True, is_train=True):
     with open(file, encoding="utf8") as f:
         for line in f:
             if header:
-                header =False
+                header = False
                 continue
-            blocks = line.strip().split('\t')
-            if is_train and len(blocks) < 4: continue
-            if not is_train: assert len(blocks) == 3
+            blocks = line.strip().split("\t")
+            if is_train and len(blocks) < 4:
+                continue
+            if not is_train:
+                assert len(blocks) == 3
             lab = "not_entailment"
             if is_train:
                 lab = blocks[-1]
-                sample = {'uid': int(blocks[0]), 'premise': blocks[-3], 'hypothesis': blocks[-2], 'label': lab}
+                sample = {
+                    "uid": int(blocks[0]),
+                    "premise": blocks[-3],
+                    "hypothesis": blocks[-2],
+                    "label": lab,
+                }
             else:
-                sample = {'uid': int(blocks[0]), 'premise': blocks[-2], 'hypothesis': blocks[-1], 'label': lab}
+                sample = {
+                    "uid": int(blocks[0]),
+                    "premise": blocks[-2],
+                    "hypothesis": blocks[-1],
+                    "label": lab,
+                }
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_wnli(file, header=True, is_train=True):
     rows = []
@@ -148,20 +188,23 @@ def load_wnli(file, header=True, is_train=True):
     with open(file, encoding="utf8") as f:
         for line in f:
             if header:
-                header =False
+                header = False
                 continue
-            blocks = line.strip().split('\t')
-            if is_train and len(blocks) < 4: continue
-            if not is_train: assert len(blocks) == 3
+            blocks = line.strip().split("\t")
+            if is_train and len(blocks) < 4:
+                continue
+            if not is_train:
+                assert len(blocks) == 3
             lab = 0
             if is_train:
                 lab = int(blocks[-1])
-                sample = {'uid': cnt, 'premise': blocks[-3], 'hypothesis': blocks[-2], 'label': lab}
+                sample = {"uid": cnt, "premise": blocks[-3], "hypothesis": blocks[-2], "label": lab}
             else:
-                sample = {'uid': cnt, 'premise': blocks[-2], 'hypothesis': blocks[-1], 'label': lab}
+                sample = {"uid": cnt, "premise": blocks[-2], "hypothesis": blocks[-1], "label": lab}
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_diag(file, header=True):
     rows = []
@@ -171,12 +214,18 @@ def load_diag(file, header=True):
             if header:
                 header = False
                 continue
-            blocks = line.strip().split('\t')
+            blocks = line.strip().split("\t")
             assert len(blocks) > 3
-            sample = {'uid': cnt, 'premise': blocks[-3], 'hypothesis': blocks[-2], 'label': blocks[-1]}
+            sample = {
+                "uid": cnt,
+                "premise": blocks[-3],
+                "hypothesis": blocks[-2],
+                "label": blocks[-1],
+            }
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_sst(file, header=True, is_train=True):
     rows = []
@@ -186,18 +235,20 @@ def load_sst(file, header=True, is_train=True):
             if header:
                 header = False
                 continue
-            blocks = line.strip().split('\t')
-            if is_train and len(blocks) < 2: continue
+            blocks = line.strip().split("\t")
+            if is_train and len(blocks) < 2:
+                continue
             lab = 0
             if is_train:
                 lab = int(blocks[-1])
-                sample = {'uid': cnt, 'premise': blocks[0], 'label': lab}
+                sample = {"uid": cnt, "premise": blocks[0], "label": lab}
             else:
-                sample = {'uid': int(blocks[0]), 'premise': blocks[1], 'label': lab}
+                sample = {"uid": int(blocks[0]), "premise": blocks[1], "label": lab}
 
             cnt += 1
             rows.append(sample)
     return rows
+
 
 def load_cola(file, header=True, is_train=True):
     rows = []
@@ -207,17 +258,19 @@ def load_cola(file, header=True, is_train=True):
             if header:
                 header = False
                 continue
-            blocks = line.strip().split('\t')
-            if is_train and len(blocks) < 2: continue
+            blocks = line.strip().split("\t")
+            if is_train and len(blocks) < 2:
+                continue
             lab = 0
             if is_train:
                 lab = int(blocks[1])
-                sample = {'uid': cnt, 'premise': blocks[-1], 'label': lab}
+                sample = {"uid": cnt, "premise": blocks[-1], "label": lab}
             else:
-                sample = {'uid': cnt, 'premise': blocks[-1], 'label': lab}
+                sample = {"uid": cnt, "premise": blocks[-1], "label": lab}
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_sts(file, header=True, is_train=True):
     rows = []
@@ -227,17 +280,28 @@ def load_sts(file, header=True, is_train=True):
             if header:
                 header = False
                 continue
-            blocks = line.strip().split('\t')
+            blocks = line.strip().split("\t")
             assert len(blocks) > 8
             score = "0.0"
             if is_train:
                 score = blocks[-1]
-                sample = {'uid': cnt, 'premise': blocks[-3],'hypothesis': blocks[-2], 'label': score}
+                sample = {
+                    "uid": cnt,
+                    "premise": blocks[-3],
+                    "hypothesis": blocks[-2],
+                    "label": score,
+                }
             else:
-                sample = {'uid': cnt, 'premise': blocks[-2],'hypothesis': blocks[-1], 'label': score}
+                sample = {
+                    "uid": cnt,
+                    "premise": blocks[-2],
+                    "hypothesis": blocks[-1],
+                    "label": score,
+                }
             rows.append(sample)
             cnt += 1
     return rows
+
 
 def load_qnnli(file, header=True, is_train=True):
     """QNLI for ranking"""
@@ -246,12 +310,13 @@ def load_qnnli(file, header=True, is_train=True):
     cnt = 0
     with open(file, encoding="utf8") as f:
         lines = f.readlines()
-        if header: lines = lines[1:]
+        if header:
+            lines = lines[1:]
 
         assert len(lines) % 2 == 0
         for idx in range(0, len(lines), 2):
-            block1 = lines[idx].strip().split('\t')
-            block2 = lines[idx + 1].strip().split('\t')
+            block1 = lines[idx].strip().split("\t")
+            block2 = lines[idx + 1].strip().split("\t")
             # train shuffle
             assert len(block1) > 2 and len(block2) > 2
             if is_train and block1[1] != block2[1]:
@@ -273,26 +338,31 @@ def load_qnnli(file, header=True, is_train=True):
             assert "," not in lab2
             assert "," not in block1[0]
             assert "," not in block2[0]
-            sample = {'uid': cnt, 'ruid': "%s,%s" % (block1[0], block2[0]), 'premise': block1[1], 'hypothesis': [block1[2], block2[2]],
-                      'label': "%s,%s" % (lab1, lab2)}
+            sample = {
+                "uid": cnt,
+                "ruid": "%s,%s" % (block1[0], block2[0]),
+                "premise": block1[1],
+                "hypothesis": [block1[2], block2[2]],
+                "label": "%s,%s" % (lab1, lab2),
+            }
             cnt += 1
             rows.append(sample)
     return rows
 
 
 def submit(path, data, label_dict=None):
-    header = 'index\tprediction'
-    with open(path ,'w') as writer:
-        predictions, uids = data['predictions'], data['uids']
-        writer.write('{}\n'.format(header))
+    header = "index\tprediction"
+    with open(path, "w") as writer:
+        predictions, uids = data["predictions"], data["uids"]
+        writer.write("{}\n".format(header))
         assert len(predictions) == len(uids)
         # sort label
         paired = [(int(uid), predictions[idx]) for idx, uid in enumerate(uids)]
         paired = sorted(paired, key=lambda item: item[0])
         for uid, pred in paired:
             if label_dict is None:
-                writer.write('{}\t{}\n'.format(uid, pred))
+                writer.write("{}\t{}\n".format(uid, pred))
             else:
                 assert type(pred) is int
-                writer.write('{}\t{}\n'.format(uid, label_dict[pred]))
+                writer.write("{}\t{}\n".format(uid, label_dict[pred]))
 
