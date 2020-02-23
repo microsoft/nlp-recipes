4036f6ff3d82ec5b9a8baf0d3eb0bd70ef17dd7b
hlums <honglu@microsoft.com>
2020-02-23 20:45:11 +0000

    Add s2s abstractive summarization modules.

--- utils_nlp/models/__init__.py
+++ utils_nlp/models/__init__.py
@@ -1,1 +1,1 @@
-from transformers.abstractive_summarization_seq2seq import S2SAbsSumProcessor, S2SAbstractiveSummarizer
+from .transformers.abstractive_summarization_seq2seq import S2SAbsSumProcessor, S2SAbstractiveSummarizer
