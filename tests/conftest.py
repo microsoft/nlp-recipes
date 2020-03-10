f57d088206fef6795b31dd25fc68b439075a658f
Daisy Deng <daden@microsoft.com>
2020-03-10 15:16:58 +0000

    rm files that are not included for this pr; add integration test in conftest; clean up unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -110,7 +110,9 @@ def notebooks():
             "abstractive_summarization_unilm_cnndm.ipynb",
         ),
         "abstractive_summarization_bertsumabs_cnndm": os.path.join(
-            folder_notebooks, "text_summarization", "abstractive_summarization_bertsumabs_cnndm.ipynb"
+            folder_notebooks,
+            "text_summarization",
+            "abstractive_summarization_bertsumabs_cnndm.ipynb",
         ),
     }
     return paths
--- tests/unit/test_abstractive_summarization_bertsum.py
+++ tests/unit/test_abstractive_summarization_bertsum.py
@@ -9,18 +9,18 @@ import pytest
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
+
 torch.set_printoptions(threshold=5000)
 from tempfile import TemporaryDirectory
 
-from utils_nlp.models.transformers.datasets import (
-    SummarizationDataset,
+from utils_nlp.models.transformers.datasets import SummarizationDataset
+from utils_nlp.models.transformers.abstractive_summarization_bertsum import (
+    BertSumAbs,
+    BertSumAbsProcessor,
+    validate,
 )
-from utils_nlp.models.transformers.abstractive_summarization_bertsum import BertSumAbs, BertSumAbsProcessor, validate
 from utils_nlp.eval.evaluate_summarization import get_rouge
 
-#CACHE_PATH = tmp_module #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
-#DATA_PATH = tmp_module  #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
-#MODEL_PATH = tmp_module #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
 
 # @pytest.fixture()
 def source_data():
@@ -29,7 +29,7 @@ def source_data():
             "Boston, MA welcome to Microsoft/nlp",
             "Welcome to text summarization.",
             "Welcome to Microsoft NERD.",
-            "Look outside, what a beautiful Charlse River fall view."
+            "Look outside, what a beautiful Charlse River fall view.",
         ],
         ["I am just another test case"],
         ["want to test more"],
@@ -39,7 +39,11 @@ def source_data():
 # @pytest.fixture()
 def target_data():
     return [
-        ["welcome to microsoft/nlp.", "Welcome to text summarization.", "Welcome to Microsoft NERD."],
+        [
+            "welcome to microsoft/nlp.",
+            "Welcome to text summarization.",
+            "Welcome to Microsoft NERD.",
+        ],
         ["I am just another test summary"],
         ["yest, I agree"],
     ]
@@ -57,13 +61,13 @@ def test_dataset_for_bertsumabs(tmp_module):
     target_file = os.path.join(tmp_module, "target.txt")
     f = open(source_file, "w")
     for i in source:
-        f.write(' '.join(i))
-        f.write('\n')
+        f.write(" ".join(i))
+        f.write("\n")
     f.close()
     f = open(target_file, "w")
     for i in target:
-        f.write(' '.join(i))
-        f.write('\n')
+        f.write(" ".join(i))
+        f.write("\n")
     f.close()
     train_dataset = SummarizationDataset(
         source_file,
@@ -85,82 +89,30 @@ def test_dataset_for_bertsumabs(tmp_module):
     return train_dataset, test_dataset
 
 
-"""
-@pytest.fixture(scope="module")
-def test_dataset_for_bertsumabs(tmp_module):
-    CACHE_PATH = tmp_module #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
-
-
-    source = source_data()
-    target = target_data()
-    processor = BertSumAbsProcessor(cache_dir=CACHE_PATH)
-    train_dataset = SummarizationNonIterableDataset(source, target)
-    test_dataset = SummarizationNonIterableDataset(source, target)
-    batch = processor.collate(train_dataset, 512, "cuda:0")
-    assert len(batch.src) == 3
-    return train_dataset, test_dataset
-"""
-
-def shorten_dataset(dataset, top_n=-1):
-    if top_n == -1:
-        return dataset
-    return SummarizationDataset(dataset.source[0:top_n], dataset.target[0:top_n])
-
-#def finetuned_model():
-#    return os.path.join(MODEL_PATH, "dist_extsum_model.pt_step13000")
-#    # return os.path.join(MODEL_PATH, "new_model_step_148000_torch1.4.0.pt")
-
-"""
-#def preprocess_cnndm_abs(top_n=8, need_process=True):
-    if need_process is False:
-        top_n = "full"
-    train_data_path = os.path.join(DATA_PATH, "train_abssum_dataset_{}.pt".format(top_n))
-    test_data_path = os.path.join(DATA_PATH, "test_abssum_dataset_{}.pt".format(top_n))
-    if need_process:
-        print("processing data")
-        train_dataset, test_dataset = CNNDMSummarizationDataset(
-            top_n=TOP_N, local_cache_path=DATA_PATH, prepare_extractive=False
-        )
-        source = [x[0] for x in list(test_dataset.get_source())]
-        target = [x[0] for x in list(test_dataset.get_target())]
-        test_sum_dataset = SummarizationNonIterableDataset(source, target)
-
-        source = [x[0] for x in list(train_dataset.get_source())]
-        target = [x[0] for x in list(train_dataset.get_target())]
-        train_sum_dataset = SummarizationNonIterableDataset(source, target)
-        
-        torch.save(train_sum_dataset, train_data_path)
-        torch.save(test_sum_dataset, test_data_path)
-
-    else:
-        train_sum_dataset = torch.load(train_data_path)
-        test_sum_dataset = torch.load(test_data_path)
-    return train_sum_dataset, test_sum_dataset
-"""
-
 @pytest.mark.gpu
 @pytest.fixture()
 def test_train_model(tmp_module, test_dataset_for_bertsumabs, batch_size=1):
-    #return "/dadendev/nlp-recipes/examples/text_summarization/abstemp/dist_extsum_model.pt_step50000"
-    CACHE_PATH = tmp_module #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
-    DATA_PATH = tmp_module  #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
-    MODEL_PATH = tmp_module #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
-
+    CACHE_PATH = (
+        tmp_module  
+    )
+    DATA_PATH = (
+        tmp_module 
+    )
+    MODEL_PATH = (
+        tmp_module
+    )
 
     processor = BertSumAbsProcessor(cache_dir=CACHE_PATH)
-    summarizer = BertSumAbs(
-        processor, cache_dir=CACHE_PATH
-    )
- 
+    summarizer = BertSumAbs(processor, cache_dir=CACHE_PATH)
+
     checkpoint = None
-    # train_sum_dataset, test_sum_dataset = preprocess_cnndm_abs(top_n=32)
     train_sum_dataset, test_sum_dataset = test_dataset_for_bertsumabs
 
     def this_validate(class_obj):
         return validate(class_obj, test_sum_dataset)
 
     MAX_STEP = 20
-    TOP_N=8
+    TOP_N = 8
     summarizer.fit(
         train_sum_dataset,
         batch_size=batch_size,
@@ -176,58 +128,59 @@ def test_train_model(tmp_module, test_dataset_for_bertsumabs, batch_size=1):
         validation_function=this_validate,
         fp16=False,
         fp16_opt_level="O1",
-        checkpoint=checkpoint
+        checkpoint=checkpoint,
     )
-    saved_model_path = os.path.join(MODEL_PATH, "summarizer_step_{}.pt".format(MAX_STEP))
+    saved_model_path = os.path.join(
+        MODEL_PATH, "summarizer_step_{}.pt".format(MAX_STEP)
+    )
     summarizer.save_model(MAX_STEP, saved_model_path)
 
     return saved_model_path
 
-@pytest.mark.gpu
-def test_finetuned_model(tmp_module, test_train_model, test_dataset_for_bertsumabs, top_n=8, batch_size=1, num_gpus=4):
-    CACHE_PATH = tmp_module #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
-    DATA_PATH = tmp_module  #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
-    MODEL_PATH = tmp_module #"/dadendev/nlp-recipes/examples/text_summarization/abstemp"
 
+@pytest.mark.gpu
+def test_finetuned_model(
+    tmp_module,
+    test_train_model,
+    test_dataset_for_bertsumabs,
+    top_n=8,
+    batch_size=1,
+    num_gpus=4,
+):
+    CACHE_PATH = (
+        tmp_module  
+    )
+    DATA_PATH = (
+        tmp_module 
+    )
+    MODEL_PATH = (
+        tmp_module
+    )
 
     # train_sum_dataset, test_sum_dataset = preprocess_cnndm_abs(need_process=False)
     train_sum_dataset, test_sum_dataset = test_dataset_for_bertsumabs
-    
+
     processor = BertSumAbsProcessor(cache_dir=CACHE_PATH)
     checkpoint = torch.load(test_train_model)
-    
+
     summarizer = BertSumAbs(
         processor,
         cache_dir=CACHE_PATH,
         test=True,
-        max_pos_length = checkpoint['max_pos_length']
+        max_pos_length=checkpoint["max_pos_length"],
     )
-    summarizer.model.load_checkpoint(checkpoint['model'])
-    """
-    summarizer.optim_bert = model_builder.build_optim_bert(
-            summarizer.model,
-            visible_gpus=None, #",".join([str(i) for i in range(num_gpus)]), #"0,1,2,3",
-            lr_bert=0.002,
-            warmup_steps_bert=20000,
-            checkpoint=None,
-        )
-    summarizer.optim_dec = model_builder.build_optim_dec(
-            summarizer.model,
-            visible_gpus=None, #",".join([str(i) for i in range(num_gpus)]), #"0,1,2,3"
-            lr_dec=0.2,
-            warmup_steps_dec=10000,
-        )
-    summarizer.amp=None
-    summarizer.save_model(20000, os.path.join(MODEL_PATH, "summarizer_step20000_with_global_step.pt"))
-    return
-    """
-    top_n = 50 #len(test_sum_dataset) + 1 
-    shortened_dataset= test_sum_dataset.shorten(top_n)
-    reference_summaries = ["".join(t).rstrip("\n") for t in shortened_dataset.get_target()]
+    summarizer.model.load_checkpoint(checkpoint["model"])
+    
+    top_n = 50  # len(test_sum_dataset) 
+    shortened_dataset = test_sum_dataset.shorten(top_n)
+    reference_summaries = [
+        "".join(t).rstrip("\n") for t in shortened_dataset.get_target()
+    ]
     print("start prediction")
     generated_summaries = summarizer.predict(
         shortened_dataset, batch_size=batch_size, num_gpus=num_gpus
     )
+
     def _write_list_to_file(list_items, filename):
         with open(filename, "w") as filehandle:
             # for cnt, line in enumerate(filehandle):
@@ -241,13 +194,3 @@ def test_finetuned_model(tmp_module, test_train_model, test_dataset_for_bertsuma
     RESULT_DIR = TemporaryDirectory().name
     rouge_score = get_rouge(generated_summaries, reference_summaries, RESULT_DIR)
     print(rouge_score)
-
-
-#test_preprocessing()
-#preprocess_cnndm_abs()
-#model_path = test_train_model()
-#test_finetuned_model(model_path)
-#test_finetuned_model(finetuned_model(),top_n=-1)
-
-#if __name__ == "__main__":
-#    main()
