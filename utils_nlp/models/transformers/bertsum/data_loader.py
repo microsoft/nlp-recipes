b30a77c1432856b80f7279305b5df10d02416943
Daisy Deng <daden@microsoft.com>
2020-03-20 19:21:01 +0000

    move iterabledistributedsampler and chunkdataloader out from extsum file

--- utils_nlp/models/transformers/bertsum/data_loader.py
+++ utils_nlp/models/transformers/bertsum/data_loader.py
@@ -2,9 +2,86 @@
 Portions Copyright (c) Microsoft Corporation
 """
 import gc
+import itertools
 import random
-
 import torch
+
+
+class IterableDistributedSampler(object):
+    """ Distributed sampler for iterable dataset.
+
+    Args:
+        world_size (int, optional): Total number of GPUs that will be used.
+            Defaults to 1.
+        rank (int, optional): Rank of the current GPU. Defaults to 0.
+        local_rank(int, optional): local_rank of the current GPU. Defaults to -1.
+
+    """
+
+    def __init__(self, world_size=1, rank=0, local_rank=-1):
+        self.world_size = world_size
+        self.rank = rank
+        self.local_rank = local_rank
+
+    def iter(self, iterable):
+        if self.local_rank == -1:
+            return iterable
+        if self.world_size > 1:
+            return itertools.islice(iterable, self.rank, None, self.world_size)
+        else:
+            return iterable
+
+
+class ChunkDataLoader(object):
+    """ Data Loader for Chunked Dataset.
+
+    Args:
+        datasets (list): list of data item list.
+        batch_size (int): Number of tokens per batch.
+        shuffle (bool): Whether the data is shuffled.
+        is_labeled (bool): Whether the data is labeled.
+        sampler (obj): Data sampler.
+
+    """
+
+    def __init__(self, datasets, batch_size, shuffle, is_labeled, sampler):
+        self.datasets = datasets
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.is_labeled = is_labeled
+        self.cur_iter = self._next_dataset_iterator(datasets)
+        assert self.cur_iter is not None
+        self.sampler = sampler
+
+    def eachiter(self):
+        dataset_iter = (d for d in self.datasets)
+        while self.cur_iter is not None:
+            for batch in self.cur_iter:
+                yield batch
+            self.cur_iter = self._next_dataset_iterator(dataset_iter)
+
+    def __iter__(self):
+        return self.sampler.iter(self.eachiter())
+
+    def _next_dataset_iterator(self, dataset_iter):
+        try:
+            # Drop the current dataset for decreasing memory
+            if hasattr(self, "cur_dataset"):
+                self.cur_dataset = None
+                gc.collect()
+                del self.cur_dataset
+                gc.collect()
+
+            self.cur_dataset = next(dataset_iter)
+        except StopIteration:
+            return None
+
+        return DataIterator(
+            dataset=self.cur_dataset,
+            batch_size=self.batch_size,
+            shuffle=self.shuffle,
+            is_labeled=self.is_labeled,
+        )
 
 
 class Batch(object):
@@ -16,39 +93,40 @@ class Batch(object):
 
     def __init__(self, data=None, is_labeled=False):
         """Create a Batch from a list of examples."""
-        if data is not None:
-            self.batch_size = len(data)
-            pre_src = [x[0] for x in data]
-            pre_segs = [x[2] for x in data]
-            pre_clss = [x[3] for x in data]
+        if data is None or len(data) == 0:
+            raise ValueError("data is empty")
+        self.batch_size = len(data)
+        pre_src = [x[0] for x in data]
+        pre_segs = [x[2] for x in data]
+        pre_clss = [x[3] for x in data]
 
-            src = torch.tensor(self._pad(pre_src, 0))
+        src = torch.tensor(self._pad(pre_src, 0))
 
-            pre_labels = None
-            labels = None
-            if is_labeled:
-                pre_labels = [x[1] for x in data]
-                labels = torch.tensor(self._pad(pre_labels, 0))
-            segs = torch.tensor(self._pad(pre_segs, 0))
-            mask = ~(src == 0)
+        pre_labels = None
+        labels = None
+        if is_labeled:
+            pre_labels = [x[1] for x in data]
+            labels = torch.tensor(self._pad(pre_labels, 0))
+        segs = torch.tensor(self._pad(pre_segs, 0))
+        mask = ~(src == 0)
 
-            clss = torch.tensor(self._pad(pre_clss, -1))
-            mask_cls = ~(clss == -1)
-            clss[clss == -1] = 0
+        clss = torch.tensor(self._pad(pre_clss, -1))
+        mask_cls = ~(clss == -1)
+        clss[clss == -1] = 0
 
-            setattr(self, "clss", clss)
-            setattr(self, "mask_cls", mask_cls)
-            setattr(self, "src", src)
-            setattr(self, "segs", segs)
-            setattr(self, "mask", mask)
+        setattr(self, "clss", clss)
+        setattr(self, "mask_cls", mask_cls)
+        setattr(self, "src", src)
+        setattr(self, "segs", segs)
+        setattr(self, "mask", mask)
 
-            src_str = [x[4] for x in data]
-            setattr(self, "src_str", src_str)
+        src_str = [x[4] for x in data]
+        setattr(self, "src_str", src_str)
 
-            if is_labeled:
-                setattr(self, "labels", labels)
-                tgt_str = [x[5] for x in data]
-                setattr(self, "tgt_str", tgt_str)
+        if is_labeled:
+            setattr(self, "labels", labels)
+            tgt_str = [x[5] for x in data]
+            setattr(self, "tgt_str", tgt_str)
 
     def to(self, device):
         src = self.src.to(device)
@@ -72,7 +150,7 @@ class Batch(object):
         return self.batch_size
 
 
-def batch(data, batch_size):
+def create_batch_with_size(data, batch_size):
     """Yield elements from data in chunks of batch_size."""
     minibatch, size_so_far = [], 0
     for ex in data:
@@ -101,49 +179,12 @@ def simple_batch_size_fn(new, count):
     return src_elements
 
 
-class Dataloader(object):
-    def __init__(self, datasets, batch_size, shuffle, is_labeled):
-        self.datasets = datasets
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.is_labeled = is_labeled
-        self.cur_iter = self._next_dataset_iterator(datasets)
-
-        assert self.cur_iter is not None
-
-    def __iter__(self):
-        dataset_iter = (d for d in self.datasets)
-        while self.cur_iter is not None:
-            for batch in self.cur_iter:
-                yield batch
-            self.cur_iter = self._next_dataset_iterator(dataset_iter)
-
-    def _next_dataset_iterator(self, dataset_iter):
-        try:
-            # Drop the current dataset for decreasing memory
-            if hasattr(self, "cur_dataset"):
-                self.cur_dataset = None
-                gc.collect()
-                del self.cur_dataset
-                gc.collect()
-
-            self.cur_dataset = next(dataset_iter)
-        except StopIteration:
-            return None
-
-        return DataIterator(
-            dataset=self.cur_dataset,
-            batch_size=self.batch_size,
-            shuffle=self.shuffle,
-            is_labeled=self.is_labeled,
-        )
-
-
 class DataIterator(object):
     def __init__(self, dataset, batch_size, is_labeled=False, shuffle=True, sort=True):
-        self.batch_size, self.is_labeled, self.dataset = batch_size, is_labeled, dataset
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.is_labeled = is_labeled
         self.iterations = 0
-        # self.device = device
         self.shuffle = shuffle
         self.sort = sort
 
@@ -204,7 +245,7 @@ class DataIterator(object):
                 p_batch = sorted(buffer, key=lambda x: len(x[3]))
             else:
                 p_batch = buffer
-            p_batch = batch(p_batch, self.batch_size)
+            p_batch = create_batch_with_size(p_batch, self.batch_size)
 
             p_batch = list(p_batch)
             if self.shuffle:
@@ -221,7 +262,8 @@ class DataIterator(object):
                     continue
                 self.iterations += 1
                 self._iterations_this_epoch += 1
+                # if len(minibatch) == 0:
+                #    continue
                 batch = Batch(minibatch, self.is_labeled)
-
                 yield batch
             return
