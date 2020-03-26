c2503a35dfdbd593fe7e9f0efb66d9c7e7e70531
Daisy Deng <daden@microsoft.com>
2020-03-26 20:05:57 +0000

    remove __iinit__.py in models

--- utils_nlp/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .transformers.abstractive_summarization_seq2seq import (
-    S2SAbsSumProcessor,
-    S2SAbstractiveSummarizer,
-    S2SAbsSumDataset,
-    S2SAbsSumProcessor,
-    S2SConfig,
-)
